50901e52f7ad6857f0291d7c94d3d72889d10894
jknapp25 <joshknappp@gmail.com>
2023-02-27 15:33:38 -0800

    conditionally add curexa org id

--- apps/app/src/configs/fulfillment.ts
+++ apps/app/src/configs/fulfillment.ts
@@ -3,7 +3,9 @@ export const fulfillmentConfig = {
     sendOrder: true,
     pickUp: true,
     mailOrder: true,
-    mailOrderProviders: [process.env.REACT_APP_CUREXA_ORG_ID],
+    mailOrderProviders: [
+      ...(process.env.REACT_APP_CUREXA_ORG_ID ? [process.env.REACT_APP_CUREXA_ORG_ID] : [])
+    ],
     sendToPatient: false,
     sendToPatientUsers: [] // its weird to have this here, but its temporary and keeps ts from complaining
   },
