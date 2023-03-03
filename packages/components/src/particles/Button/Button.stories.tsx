45e93183e37b48d23742c3fc57ecaeee599e674c
Paul Christophe <paul.christophe6@gmail.com>
2023-03-03 13:11:00 -0500

    poc themes

--- packages/components/src/particles/Button/Button.stories.tsx
+++ packages/components/src/particles/Button/Button.stories.tsx
@@ -3,11 +3,19 @@ import Button, { ButtonProps } from '../Button';
 import type { Meta, StoryObj } from '@storybook/html';
 import type { ComponentProps } from 'solid-js';
 
-type Story = StoryObj<ButtonProps>;
+type Theme = {
+  theme: 'theme-photon' | 'theme-weekend';
+};
+
+type ButtonAndTheme = ButtonProps & Theme;
+
+type Story = StoryObj<ButtonAndTheme>;
 
 export const Default: Story = {
   args: {
-    variant: 'primary'
+    variant: 'primary',
+    size: 'lg',
+    theme: 'theme-photon'
   }
 };
 
@@ -17,16 +25,32 @@ export default {
   argTypes: {
     variant: {
       options: ['primary', 'secondary'],
+      defaultValue: 'primary',
       control: {
         type: 'select'
       }
     },
     size: {
       options: ['xl', 'lg', 'md', 'sm', 'xs'],
+      defaultValue: 'lg',
+      control: {
+        type: 'select'
+      }
+    },
+    theme: {
+      options: ['theme-photon', 'theme-weekend'],
+      defaultValue: 'photon',
       control: {
         type: 'select'
       }
     }
   },
-  render: (props) => <Button {...props}>hello</Button>
+  render: (props) => {
+    const { theme, ...rest } = props;
+    return (
+      <div class={theme}>
+        <Button {...rest}>Hello</Button>
+      </div>
+    );
+  }
 } as Meta<ComponentProps<typeof Button>>;
--- packages/components/src/particles/Button/index.tsx
+++ packages/components/src/particles/Button/index.tsx
@@ -25,13 +25,15 @@ export default function Button({
   }
 
   const buttonClasses = clsx(
-    'rounded',
-    'text-white',
+    ['xl', 'lg', 'md'].includes(size) ? 'rounded-md' : 'rounded',
+    'font-sans box-border',
+    variant === 'primary' ? 'text-primaryText' : 'text-secondaryText',
+    variant === 'secondary' ? 'border-2 border-primary600' : 'border-2 border-transparent',
     'shadow-sm',
     'focus-visible:outline',
     'focus-visible:outline-2',
     'focus-visible:outline-offset-2',
-    variant === 'primary' ? 'bg-indigo-600' : 'bg-gray-400',
+    variant === 'primary' ? 'bg-primary600' : 'bg-secondary50',
     size === 'xl' && 'text-lg py-4 px-6',
     size === 'lg' && 'text-base py-3 px-4',
     size === 'md' && 'text-sm py-2 px-3',
@@ -39,7 +41,7 @@ export default function Button({
     size === 'xs' && 'text-xs py-1 px-2',
     disabled
       ? 'opacity-50 cursor-not-allowed'
-      : 'hover:bg-indigo-500 focus-visible:outline-indigo-600'
+      : 'hover:bg-primary500 focus-visible:outline-primary600'
   );
 
   return (
