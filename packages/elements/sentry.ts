7f00d76a84a00a8284d4120e05a5796b0f562f75
Paul Christophe <paul.christophe6@gmail.com>
2023-03-06 17:53:30 -0500

    node env sentry

--- packages/elements/sentry.ts
+++ packages/elements/sentry.ts
@@ -4,6 +4,7 @@ import pjson from './package.json';
 
 Sentry.init({
   dsn: 'https://1d34034cd858470ba77ff10172425af2@o1356305.ingest.sentry.io/4504792337219584',
+  environment: process.env.NODE_ENV,
   release: pjson.version,
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0
