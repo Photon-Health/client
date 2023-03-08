b5874f952d81c8a2d6fc3fa3034da11fc3f51e23
Paul Christophe <paul.christophe6@gmail.com>
2023-03-08 13:07:45 -0500

    update app component

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -3,22 +3,18 @@ import Button from './particles/Button';
 const App = () => {
   return (
     <article class="prose lg:prose-lg p-4">
-      <h1 class="mb-0">Button</h1>
-
+      {' '}
+      <h1 class="mb-0">Buttons</h1>
       <h3>Variants</h3>
       <div class="flex flex-wrap items-center justify-left gap-2 overflow-x-hidden bg-top">
         <Button variant="primary">Primary</Button>
         <Button variant="secondary">Secondary</Button>
-        <Button variant="accent">Accent</Button>
-        <Button variant="info">Info</Button>
-        <Button variant="success">Success</Button>
-        <Button variant="warning">Warning</Button>
-        <Button variant="error">Error</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="link">Link</Button>
       </div>
       <h3>Sizes</h3>
       <div class="flex flex-wrap items-center justify-left gap-2 overflow-x-hidden bg-top">
+        <Button variant="primary" size="xl">
+          X-Large
+        </Button>
         <Button variant="primary" size="lg">
           Large
         </Button>
@@ -30,37 +26,6 @@ const App = () => {
         </Button>
         <Button variant="primary" size="xs">
           Extra Small
-        </Button>
-      </div>
-      <h3>Outline</h3>
-      <div class="flex flex-wrap items-center justify-left gap-2 overflow-x-hidden bg-top">
-        <Button variant="primary" isOutline>
-          Primary
-        </Button>
-        <Button variant="secondary" isOutline>
-          Secondary
-        </Button>
-        <Button variant="accent" isOutline>
-          Accent
-        </Button>
-        <Button variant="info" isOutline>
-          Info
-        </Button>
-        <Button variant="success" isOutline>
-          Success
-        </Button>
-        <Button variant="warning" isOutline>
-          Warning
-        </Button>
-        <Button variant="error" isOutline>
-          Error
-        </Button>
-      </div>
-
-      <h3>Loading</h3>
-      <div class="flex flex-wrap items-center justify-left gap-2 overflow-x-hidden bg-top">
-        <Button isLoading isDisabled>
-          Loading
         </Button>
       </div>
     </article>
