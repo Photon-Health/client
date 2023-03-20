e17157ce6402e20ca4a76fa6ceda51f701b68d9f
Paul Christophe <paul.christophe6@gmail.com>
2023-03-20 16:45:52 -0400

    wip input

--- packages/components/src/particles/Input/index.tsx
+++ packages/components/src/particles/Input/index.tsx
@@ -18,7 +18,7 @@ export default function Input(props: InputProps) {
       </label>
       <input
         {...inputProps}
-        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        class="mt-2 block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         placeholder="you@example.com"
       />
       <Show when={!!error}>
