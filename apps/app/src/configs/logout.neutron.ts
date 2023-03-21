aff37e31427298b9d59cc05d30ad1aa60f70c730
Paul Christophe <paul.christophe6@gmail.com>
2023-03-21 10:07:24 -0400

    update neutron logout settings

--- apps/app/src/configs/logout.neutron.ts
+++ apps/app/src/configs/logout.neutron.ts
@@ -4,5 +4,10 @@ export const logoutSettings: LogoutSettings = {
   default: {
     returnTo: window.location.origin,
     federated: false
+  },
+  // Peachy
+  [process.env.REACT_APP_PEACHY_ORG_ID as string]: {
+    returnTo: window.location.origin,
+    federated: true
   }
 };
