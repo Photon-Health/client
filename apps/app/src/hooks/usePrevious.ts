46e7be0881cb66f75d6873965ead3ffed543f643
Paul Christophe <paul.christophe6@gmail.com>
2023-04-10 15:11:30 -0400

    better types

--- apps/app/src/hooks/usePrevious.ts
+++ apps/app/src/hooks/usePrevious.ts
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 
-export default function usePrevious(value: any) {
-  const ref = useRef();
+export default function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T | undefined>();
 
   useEffect(() => {
     ref.current = value;
