cb632bae55e1823285a99fde1bcdcb961f37c756
Paul Christophe <paul.christophe6@gmail.com>
2023-04-12 09:31:45 -0400

    split button props

--- packages/components/src/particles/Button/index.tsx
+++ packages/components/src/particles/Button/index.tsx
@@ -1,5 +1,5 @@
 import { clsx } from 'clsx';
-import { JSX, mergeProps } from 'solid-js';
+import { JSX, mergeProps, splitProps } from 'solid-js';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
 export type ButtonSize = 'xl' | 'lg' | 'md' | 'sm' | 'xs';
@@ -11,25 +11,26 @@ export interface ButtonProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement>
 
 export default function Button(props: ButtonProps) {
   const merged = mergeProps({ variant: 'primary', size: 'lg' }, props);
+  const [otherProps, buttonProps] = splitProps(merged, ['variant', 'size', 'children']);
 
   const buttonClasses = clsx('font-semibold shadow-sm', {
-    'rounded-md': ['xl', 'lg', 'md'].includes(merged.size),
-    rounded: !['xl', 'lg', 'md'].includes(merged.size),
+    'rounded-md': ['xl', 'lg', 'md'].includes(otherProps.size),
+    rounded: !['xl', 'lg', 'md'].includes(otherProps.size),
     'text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-indigo-500':
-      merged.variant === 'primary',
+      otherProps.variant === 'primary',
     'rounded bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50':
-      merged.variant === 'secondary',
-    'bg-indigo-50 text-indigo-600 hover:bg-indigo-100': merged.variant === 'tertiary',
-    'text-sm px-3.5 py-2.5': merged.size === 'xl',
-    'text-sm px-3 py-2': merged.size === 'lg',
-    'text-sm px-2.5 py-1.5': merged.size === 'md',
-    'text-sm px-2 py-1': merged.size === 'sm',
-    'text-xs px-2 py-1': merged.size === 'xs',
-    'opacity-50 cursor-not-allowed': merged.disabled
+      otherProps.variant === 'secondary',
+    'bg-indigo-50 text-indigo-600 hover:bg-indigo-100': otherProps.variant === 'tertiary',
+    'text-sm px-3.5 py-2.5': otherProps.size === 'xl',
+    'text-sm px-3 py-2': otherProps.size === 'lg',
+    'text-sm px-2.5 py-1.5': otherProps.size === 'md',
+    'text-sm px-2 py-1': otherProps.size === 'sm',
+    'text-xs px-2 py-1': otherProps.size === 'xs',
+    'opacity-50 cursor-not-allowed': buttonProps.disabled
   });
 
   return (
-    <button class={buttonClasses} type="button" {...merged}>
+    <button class={buttonClasses} type="button" {...buttonProps}>
       {merged.children}
     </button>
   );
