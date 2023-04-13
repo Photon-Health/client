5ec81edeebb129b7ffa5919dd1d6ee91b33f4a68
jknapp25 <joshknappp@gmail.com>
2023-04-13 09:21:23 -0700

    Delivery address. Capsule tagline.

--- apps/patient/src/components/CourierOptions.tsx
+++ apps/patient/src/components/CourierOptions.tsx
@@ -1,4 +1,13 @@
-import { Card, CardBody, Heading, Image, SlideFade, Text, VStack } from '@chakra-ui/react'
+import {
+  Card,
+  CardBody,
+  Heading,
+  Image,
+  SlideFade,
+  Text,
+  VStack,
+  useBreakpointValue
+} from '@chakra-ui/react'
 
 import t from '../utils/text.json'
 
@@ -10,9 +19,18 @@ interface Props {
   location: string
   selectedId: string
   handleSelect: Function
+  patientAddress: string
 }
 
-export const CourierOptions = ({ capsule, location, selectedId, handleSelect }: Props) => {
+export const CourierOptions = ({
+  capsule,
+  location,
+  selectedId,
+  handleSelect,
+  patientAddress
+}: Props) => {
+  const isMobile = useBreakpointValue({ base: true, md: false })
+
   if (!location) return null
 
   const courierOptions = [
@@ -20,35 +38,38 @@ export const CourierOptions = ({ capsule, location, selectedId, handleSelect }: 
       id: process.env.REACT_APP_CAPSULE_PHARMACY_ID,
       logo: capsuleLogo,
       enabled: capsule,
-      description: 'Specialty medications delivered.'
+      description: 'Free, same-day prescription delivery.'
     }
     // add additional mail order options here
   ]
 
   return (
-    <SlideFade offsetY="60px" in={true}>
-      <VStack spacing={3} align="span" w="full">
+    <VStack spacing={3} align="span" w="full">
+      <SlideFade offsetY="60px" in={true}>
         <VStack spacing={1} align="start">
           <Heading as="h5" size="sm">
             {t.pharmacy.courier.heading}
           </Heading>
-          <Text>{t.pharmacy.courier.subheading}</Text>
+          <Text size="sm">
+            {t.pharmacy.courier.subheading} {patientAddress}
+          </Text>
         </VStack>
+      </SlideFade>
 
-        {courierOptions.map((ph) => {
-          if (!ph.enabled) return null
-          return (
+      {courierOptions.map((ph) => {
+        if (!ph.enabled) return null
+        return (
+          <SlideFade offsetY="60px" in={true} key={`courier-pharmacy-${ph.id}`}>
             <Card
-              key={`courier-pharmacy-${ph.id}`}
-              w="full"
               bgColor="white"
               cursor="pointer"
               onClick={() => handleSelect(ph.id)}
               border="2px solid"
               borderColor={selectedId === ph.id ? 'brand.600' : 'white'}
+              mx={isMobile ? -3 : undefined}
             >
               <CardBody p={3}>
-                <VStack me="auto" align="start" spacing={2}>
+                <VStack align="start" spacing={2}>
                   <Image src={ph.logo} width="auto" height="25px" />
                   <Text fontSize="sm" color="gray.500">
                     {ph.description}
@@ -56,10 +77,10 @@ export const CourierOptions = ({ capsule, location, selectedId, handleSelect }: 
                 </VStack>
               </CardBody>
             </Card>
-          )
-        })}
-      </VStack>
-    </SlideFade>
+          </SlideFade>
+        )
+      })}
+    </VStack>
   )
 }
 
