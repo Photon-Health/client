21a9485a402d64240bfb59086cfe71019370bf25
Paul Christophe <paul.christophe6@gmail.com>
2023-04-14 15:02:26 -0500

    fix naked button

--- packages/components/src/particles/Button/index.tsx
+++ packages/components/src/particles/Button/index.tsx
@@ -18,17 +18,19 @@ export default function Button(props: ButtonProps) {
     'shadow-sm': otherProps.variant !== 'naked',
     'rounded-md': ['xl', 'lg', 'md'].includes(otherProps.size),
     rounded: !['xl', 'lg', 'md'].includes(otherProps.size),
-    'text-sm px-3.5 py-2.5': otherProps.size === 'xl',
-    'text-sm px-3 py-2': otherProps.size === 'lg',
-    'text-sm px-2.5 py-1.5': otherProps.size === 'md',
-    'text-sm px-2 py-1': otherProps.size === 'sm',
-    'text-xs px-2 py-1': otherProps.size === 'xs',
+    'text-xs': otherProps.size === 'xs',
+    'text-sm': otherProps.size !== 'xs',
+    'px-3.5 py-2.5': otherProps.size === 'xl' && otherProps.variant !== 'naked',
+    'px-3 py-2': otherProps.size === 'lg' && otherProps.variant !== 'naked',
+    'px-2.5 py-1.5': otherProps.size === 'md' && otherProps.variant !== 'naked',
+    'px-2 py-1':
+      (otherProps.size === 'sm' || otherProps.size === 'xs') && otherProps.variant !== 'naked',
     'text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-indigo-500':
       otherProps.variant === 'primary',
     'rounded bg-white text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50':
       otherProps.variant === 'secondary',
     'bg-indigo-50 text-indigo-600 hover:bg-indigo-100': otherProps.variant === 'tertiary',
-    'text-indigo-600 hover:text-indigo-500 bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-300 px-0 py-0':
+    'text-indigo-600 hover:text-indigo-500 bg-transparent focus:outline-none focus:ring-2 focus:ring-blue-300':
       otherProps.variant === 'naked',
     'opacity-50 cursor-not-allowed': buttonProps.disabled,
     'justify-center inline-flex items-center gap-x-1.5': otherProps?.iconLeft
