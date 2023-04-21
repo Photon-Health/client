9e6ab10a521385043012e9509246e85d7cb75378
Paul Christophe <paul.christophe6@gmail.com>
2023-04-21 18:00:14 -0400

    streamline dosage calculations

--- packages/components/src/systems/DoseCalculator/utils/doseTools.ts
+++ packages/components/src/systems/DoseCalculator/utils/doseTools.ts
@@ -2,20 +2,6 @@ export type WeightUnit = 'kgs' | 'g' | 'lbs' | 'oz';
 export type DosageUnit = 'kg' | 'lb' | 'mcg' | 'mg' | 'g';
 export type LiquidUnit = 'mcg' | 'mg' | 'g';
 
-type ConversionFactors = {
-  [key in DosageUnit]: {
-    [key in WeightUnit | LiquidUnit]?: number;
-  };
-};
-
-const conversionFactors: ConversionFactors = {
-  kg: { oz: (1 / 16) * 0.453592, lbs: 0.453592, kgs: 1, g: 1 / 1000 },
-  lb: { kgs: 2.20462, g: 2.20462 / 1000, lbs: 1, oz: 1 / 16 },
-  mcg: { mcg: 1, mg: 1 / 1000, g: 1 / (1000 * 1000) },
-  mg: { mcg: 1000, mg: 1, g: 1 / 1000 },
-  g: { mcg: 1000 * 1000, mg: 1000, g: 1 }
-};
-
 export interface Weight {
   value: number;
   unit: WeightUnit;
@@ -31,30 +17,60 @@ export interface Liquid {
   unit: LiquidUnit;
 }
 
+type ConversionFactors = {
+  [key in DosageUnit]: {
+    [key in WeightUnit | LiquidUnit]?: number;
+  };
+};
+
+const conversionFactors: ConversionFactors = {
+  kg: {
+    oz: (1 / 16) * 0.453592,
+    lbs: 0.453592,
+    kgs: 1,
+    g: 1 / 1000
+  },
+  lb: {
+    kgs: 2.20462,
+    g: 2.20462 / 1000,
+    lbs: 1,
+    oz: 1 / 16
+  },
+  mcg: {
+    mcg: 1,
+    mg: 1 / 1000,
+    g: 1 / (1000 * 1000)
+  },
+  mg: {
+    mcg: 1000,
+    mg: 1,
+    g: 1 / 1000
+  },
+  g: {
+    mcg: 1000 * 1000,
+    mg: 1000,
+    g: 1
+  }
+};
+
+export const convertUnit = (dosageUnit: DosageUnit, unit: Weight | Liquid): number => {
+  return dosageUnit in conversionFactors && unit.unit in conversionFactors[dosageUnit]
+    ? unit.value * (conversionFactors[dosageUnit][unit.unit] ?? 0)
+    : 0;
+};
+
 export const compatibleDosageWeight = (dosageUnit: DosageUnit, weightUnit: WeightUnit): boolean =>
   ((weightUnit === 'kgs' || weightUnit === 'g') && dosageUnit === 'kg') ||
   ((weightUnit === 'lbs' || weightUnit === 'oz') && dosageUnit === 'lb');
 
-export const convertCompatibleWeight = (dosageUnit: DosageUnit, weight: Weight): number => {
-  return dosageUnit in conversionFactors && weight.unit in conversionFactors[dosageUnit]
-    ? weight.value * conversionFactors[dosageUnit][weight.unit]
-    : 0;
-};
-
 export const compatibleLiquid = (dosageUnit: DosageUnit, liquidDosageUnit: LiquidUnit): boolean =>
   dosageUnit === liquidDosageUnit;
-
-export const convertCompatibleLiquid = (dosageUnit: DosageUnit, liquid: Liquid): number => {
-  return dosageUnit in conversionFactors && liquid.unit in conversionFactors[dosageUnit]
-    ? liquid.value * conversionFactors[dosageUnit][liquid.unit]
-    : 0;
-};
 
 export const calculateDosage = (dosage: Dosage, weight: Weight): number => {
   const isCompatible = compatibleDosageWeight(dosage.unit, weight.unit);
   const dosageWeight = isCompatible
-    ? weight.value * conversionFactors[dosage.unit][weight.unit]
-    : convertCompatibleWeight(dosage.unit, weight);
+    ? weight.value * (conversionFactors[dosage.unit][weight.unit] ?? 0)
+    : convertUnit(dosage.unit, weight);
 
   return dosage.value * dosageWeight;
 };
@@ -72,7 +88,7 @@ export const calculateLiquidDosage = (
 
   const drugAmount = compatibleLiquid(dosageUnit, drugUnit)
     ? drug
-    : convertCompatibleLiquid(dosageUnit, { value: drug, unit: drugUnit });
+    : convertUnit(dosageUnit, { value: drug, unit: drugUnit });
 
   return (dosage * volume) / drugAmount;
 };
