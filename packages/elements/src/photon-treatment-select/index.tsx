ca94f388b7cf4ee077f259ba475cb10570c68d52
Paul Christophe <paul.christophe6@gmail.com>
2023-05-02 11:33:46 -0400

    display template name

--- packages/elements/src/photon-treatment-select/index.tsx
+++ packages/elements/src/photon-treatment-select/index.tsx
@@ -57,16 +57,11 @@ customElement(
 
     const getData = (filter: string): (Treatment | PrescriptionTemplate)[] => {
       if (store.catalogs.data.length > 0) {
-        const data = props.offCatalogOption
-          ? [
-              props.offCatalogOption as Treatment,
-              ...store.catalogs.data[0].templates.map((x) => x as PrescriptionTemplate),
-              ...store.catalogs.data[0].treatments.map((x) => x as Treatment)
-            ]
-          : [
-              ...store.catalogs.data[0].templates.map((x) => x as PrescriptionTemplate),
-              ...store.catalogs.data[0].treatments.map((x) => x as Treatment)
-            ];
+        const data = [
+          ...(props.offCatalogOption ? [props.offCatalogOption as Treatment] : []),
+          ...store.catalogs.data[0].templates.map((x) => x as PrescriptionTemplate),
+          ...store.catalogs.data[0].treatments.map((x) => x as Treatment)
+        ];
         if (filter.length === 0) {
           return data;
         }
@@ -94,24 +89,16 @@ customElement(
           groups={[
             {
               label: 'Off Catalog',
-              filter: (x: Treatment | PrescriptionTemplate) => {
-                if (!props.offCatalogOption) {
-                  return false;
-                }
-                return x.id === props.offCatalogOption?.id;
-              }
+              filter: (t: Treatment | PrescriptionTemplate) =>
+                props.offCatalogOption && t.id === props.offCatalogOption.id
             },
             {
               label: 'Templates',
-              filter: (x: Treatment | PrescriptionTemplate) => {
-                return x ? Object.keys(x).includes('treatment') : false;
-              }
+              filter: (t: Treatment | PrescriptionTemplate) => 'treatment' in t
             },
             {
               label: 'Catalog',
-              filter: (x: Treatment | PrescriptionTemplate) => {
-                return x ? Object.keys(x).includes('name') : false;
-              }
+              filter: (t: Treatment | PrescriptionTemplate) => 'name' in t && !('treatment' in t)
             }
           ]}
           label={props.label}
@@ -123,36 +110,31 @@ customElement(
           hasMore={false}
           selectedData={props.selected ?? (props.offCatalogOption as Treatment)}
           displayAccessor={(t: Treatment | PrescriptionTemplate, groupAccess: boolean) => {
-            return 'name' in t ? (
+            return 'name' in t && !('treatment' in t) ? (
               t.name
             ) : groupAccess ? (
               <>
                 <p class="overflow-hidden whitespace-nowrap overflow-ellipsis">
+                  {t?.name ? `${t.name}: ` : ''}
                   {t.treatment.name}
                 </p>
                 <p class="font-normal pl-4 overflow-hidden whitespace-nowrap overflow-ellipsis">
-                  QTY: {t.dispenseQuantity} {t.dispenseUnit}&nbsp;|&nbsp;Days Supply:&nbsp;
-                  {t.daysSupply}
-                  &nbsp;|&nbsp;Refills:{' '}
+                  QTY: {t.dispenseQuantity} {t.dispenseUnit} | Days Supply: {t.daysSupply} |{' '}
                   {/* A "-1" is needed here because in the UI we are displaying the number of refills, not fills. We get this from the templates in the catalog */}
-                  {t.fillsAllowed ? t.fillsAllowed - 1 : 0}&nbsp;|&nbsp;Sig: {t.instructions}
+                  Refills: {t.fillsAllowed ? t.fillsAllowed - 1 : 0} | Sig: {t.instructions}
                 </p>
               </>
             ) : (
               t.treatment.name
             );
           }}
-          onSearchChange={async (s: string) => {
-            setFilter(s);
-          }}
+          onSearchChange={async (s: string) => setFilter(s)}
           onOpen={async () => {
             if (store.catalogs.data.length === 0) {
               await actions.getCatalogs(client!.getSDK());
             }
           }}
-          onHide={async () => {
-            setFilter('');
-          }}
+          onHide={async () => setFilter('')}
           noDataMsg={
             store.catalogs.data.length === 0 && !store.catalogs.isLoading
               ? 'No catalog found'
