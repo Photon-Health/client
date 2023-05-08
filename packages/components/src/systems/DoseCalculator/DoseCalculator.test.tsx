c2fe3123c3c133e623e01473e4b8987bfd210ca8
Paul Christophe <paul.christophe6@gmail.com>
2023-05-08 15:43:57 -0400

    using testing -library to interact with the component

--- packages/components/src/systems/DoseCalculator/DoseCalculator.test.tsx
+++ packages/components/src/systems/DoseCalculator/DoseCalculator.test.tsx
@@ -1,6 +1,10 @@
 import { render } from '@solidjs/testing-library';
-import { describe, expect, it } from 'vitest';
-import '@testing-library/jest-dom';
+import { describe, expect, it, vi } from 'vitest';
+// TODO
+// "Error: Cannot find module /client/packages/components/node_modules/@testing-library/jest-dom/extend-expect.js"
+// Vite isn't looking for this file in the right place, hard coding for now
+import '../../../../../node_modules/@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import DoseCalculator from '.';
 
 describe('App', () => {
@@ -11,16 +15,37 @@ describe('App', () => {
     expect(getByText('Calculate Dose Quantity')).toBeInTheDocument();
   });
 
-  it('should render the provided medication name', () => {
-    const testMedicationName = 'Test Medication';
-    const { getByText } = render(() => (
+  it('should set input values correctly', async () => {
+    const medicationName = 'Amoxicillin 200 MG in 5mL Oral Suspension, 20lbs, 80 mg/kg';
+    const setAutocompleteValuesMock = vi.fn();
+
+    const { getByText, getByLabelText } = render(() => (
       <DoseCalculator
         open={true}
         onClose={() => {}}
-        setAutocompleteValues={() => {}}
-        medicationName={testMedicationName}
+        setAutocompleteValues={setAutocompleteValuesMock}
+        medicationName={medicationName}
       />
     ));
-    expect(getByText(testMedicationName)).toBeInTheDocument();
+
+    // Check that the medication name is rendered
+    expect(getByText(medicationName)).toBeInTheDocument();
+
+    // Set input values
+    await userEvent.type(getByLabelText('Dose'), '90');
+    await userEvent.type(getByLabelText('Patient Weight'), '20');
+    await userEvent.type(getByLabelText('Liquid Concentration'), '200');
+    await userEvent.type(getByLabelText('Per Volume'), '5');
+    await userEvent.type(getByLabelText('Duration in Days'), '10');
+    await userEvent.type(getByLabelText('Doses per Day'), '2');
+
+    await userEvent.click(getByText('Autofill'));
+
+    expect(setAutocompleteValuesMock).toHaveBeenCalledTimes(1);
+    expect(setAutocompleteValuesMock).toHaveBeenCalledWith({
+      liquidDose: 10.2,
+      totalLiquid: 204.1,
+      unit: 'mL'
+    });
   });
 });
