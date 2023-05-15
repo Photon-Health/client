b1c9195f0481d0d5c729dcde091bb710432644b8
jknapp25 <joshknappp@gmail.com>
2023-05-15 08:43:21 -0700

    revert boson and neutron

--- packages/settings/src/lib/neutron.ts
+++ packages/settings/src/lib/neutron.ts
@@ -2,7 +2,7 @@ import { OrganizationSettings } from '../types';
 
 const CUREXA_ID = 'phr_01GCA54GVKA06C905DETQ9SY98';
 const CAREPOINT_ID = 'phr_01GA9HPVBVJ0E65P819FD881N0';
-// const ALTO_ID = 'phr_01G9CM93X1NFP1C9H9K50DPKHX';
+const ALTO_ID = 'phr_01G9CM93X1NFP1C9H9K50DPKHX';
 const AMAZON_PHARMACY_ID = 'phr_01GA9HPV5XYTC1NNX213VRRBZ3';
 
 /**
@@ -44,7 +44,7 @@ export const neutron: {
     mailOrder: true,
     mailOrderProviders: [CUREXA_ID, CAREPOINT_ID],
     courier: true,
-    courierProviders: []
+    courierProviders: [ALTO_ID]
   },
   // NewCo (demo's)
   org_YiUudCToTSrjOuow: {
