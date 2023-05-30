30877a955c7c721ba60548193403d40b545c5983
jknapp25 <joshknappp@gmail.com>
2023-05-30 07:35:10 -0700

    cleanup disabled check

--- apps/patient/src/views/ReadyBy.tsx
+++ apps/patient/src/views/ReadyBy.tsx
@@ -11,7 +11,8 @@ import { PoweredBy } from '../components/PoweredBy';
 import t from '../utils/text.json';
 import { OrderContext } from './Main';
 
-const currentTimeIsAfterOption = (option: string): boolean => {
+const checkDisabled = (option: string): boolean => {
+  if (option === 'tomorrow') return false;
   const currentTime = dayjs();
   const afterHoursOption = t.readyBy.options[5];
   const afterHoursStarts = '6:00 pm';
@@ -56,8 +57,7 @@ export const ReadyBy = () => {
 
           <VStack spacing={3} w="full">
             {t.readyBy.options.map((option, i) => {
-              const isDisabled =
-                i !== t.readyBy.options.length - 1 && currentTimeIsAfterOption(option);
+              const isDisabled = checkDisabled(option);
               return (
                 <Button
                   key={option}
