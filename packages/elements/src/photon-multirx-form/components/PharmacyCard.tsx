914fcb7ebfada797862b05b438016a1dfea61ae5
Paul Christophe <paul.christophe6@gmail.com>
2023-07-12 17:24:30 -0400

    upgrade to card

--- packages/elements/src/photon-multirx-form/components/PharmacyCard.tsx
+++ packages/elements/src/photon-multirx-form/components/PharmacyCard.tsx
@@ -1,9 +1,11 @@
 import { createEffect, createSignal, Show } from 'solid-js';
 import { usePhoton } from '../../context';
+import { RadioGroup, Text } from '@photonhealth/components';
+import { Pharmacy } from '@photonhealth/sdk/dist/types';
 
 export const PharmacyCard = (props: { pharmacyId: string | undefined }) => {
   const client = usePhoton();
-  const [pharmacy, setPharmacy] = createSignal({ name: '' });
+  const [pharmacy, setPharmacy] = createSignal<Pharmacy | null>(null);
 
   createEffect(async () => {
     if (props.pharmacyId && client) {
@@ -13,18 +15,34 @@ export const PharmacyCard = (props: { pharmacyId: string | undefined }) => {
       setPharmacy(data.pharmacy);
     }
   });
+
   return (
     <photon-card>
       <div class="flex flex-col gap-3">
         <div class="flex flex-row">
           <p class="font-sans text-l font-medium flex-grow">Selected Pharmacy</p>
         </div>
-        <Show when={!pharmacy()}>
-          <sl-spinner style={{ 'font-size': '1rem' }} />
-        </Show>
 
         <Show when={pharmacy()}>
-          <div class="flex flex-col gap-2">{pharmacy()?.name}</div>
+          <RadioGroup label="Pharmacies" initSelected={props?.pharmacyId}>
+            <RadioGroup.Option value={props?.pharmacyId}>
+              <div class="flex flex-col items-start">
+                <Text loading={!pharmacy()} sampleLoadingText="Loading Name">
+                  {pharmacy()?.name}
+                </Text>
+                <Text
+                  loading={!pharmacy()}
+                  color="gray"
+                  size="sm"
+                  sampleLoadingText="Cityville, ST"
+                >
+                  <span>
+                    {pharmacy()?.address?.city}, {pharmacy()?.address?.state}
+                  </span>
+                </Text>
+              </div>
+            </RadioGroup.Option>
+          </RadioGroup>
         </Show>
       </div>
     </photon-card>
