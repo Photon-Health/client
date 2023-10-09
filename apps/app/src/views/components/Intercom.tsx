6fea0a60d32848033e9d2c4d500b9bed0641b6c8
Paul Christophe <paul.christophe6@gmail.com>
2023-10-09 15:55:22 -0400

    test

--- apps/app/src/views/components/Intercom.tsx
+++ apps/app/src/views/components/Intercom.tsx
@@ -6,12 +6,26 @@ export const Intercom = () => {
 
   useEffect(() => {
     if (user) {
+      console.log('booting intercom', {
+        api_base: 'https://api-iam.intercom.io',
+        app_id: 'uqg0xvmw',
+        name: user.name,
+        email: user.email,
+        org_id: user.org_id,
+        application: 'clinical',
+        test: true,
+        test_string: 'test',
+        test_number: 123,
+        is_clinical_app: false
+      });
       window.Intercom('boot', {
         api_base: 'https://api-iam.intercom.io',
         app_id: 'uqg0xvmw',
         name: user.name,
         email: user.email,
         org_id: user.org_id,
+        is_app: false,
+        is_clinical_app: false,
         application: 'clinical',
         test: true,
         test_string: 'test',
