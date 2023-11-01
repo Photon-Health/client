0e3f4624a54103dfe2a3d08569647f516cdd8826
Paul Christophe <paul.christophe6@gmail.com>
2023-11-01 16:09:15 -0400

    remove logs

--- apps/app/src/views/components/Zendesk.tsx
+++ apps/app/src/views/components/Zendesk.tsx
@@ -23,28 +23,6 @@ export const Zendesk = () => {
       // zendesk fields are set https://photonhealthhelp.zendesk.com/admin/objects-rules/tickets/ticket-fields
       // grab the field id, then you can set the value here
       window.zE('messenger:set', 'conversationTags', ['clinical']);
-      console.log([
-        {
-          // Organization
-          id: '22439531168787',
-          value: user.org_id
-        },
-        {
-          // Customer Name
-          id: '22447880359059',
-          value: user.name
-        },
-        {
-          // Customer Email
-          id: '22448017838483',
-          value: user.email
-        },
-        {
-          // Environment
-          id: '22448619079059',
-          value: getEnvironmentFromUrl()
-        }
-      ]);
       window.zE('messenger:set', 'conversationFields', [
         {
           // Organization
