2caec9c4090c09d60891621fba386b8eefabe213
Paul Christophe <paul.christophe6@gmail.com>
2023-11-06 16:03:42 -0500

    List box triggers value

--- packages/components/src/systems/AddressForm/index.tsx
+++ packages/components/src/systems/AddressForm/index.tsx
@@ -3,6 +3,9 @@ import Input from '../../particles/Input';
 import InputGroup from '../../particles/InputGroup';
 import { states } from './states';
 import ListSelect from '../../particles/ListBox';
+import Card from '../../particles/Card';
+import Text from '../../particles/Text';
+import Button from '../../particles/Button';
 
 export default function AddressForm() {
   const { form } = createForm({
@@ -13,24 +16,30 @@ export default function AddressForm() {
   });
 
   return (
-    <form ref={form}>
-      <InputGroup label="Address Line 1 *" subLabel="Enter Street Number and Name">
-        <Input type="text" name="street1" />
-      </InputGroup>
-      <InputGroup label="Address Line 2" subLabel="Enter Apt, Unit, or Suite">
-        <Input type="text" name="street2" />
-      </InputGroup>
-      <InputGroup label="City *">
-        <Input type="text" name="city" />
-      </InputGroup>
-      <div class="grid grid-cols-2 gap-4">
-        <InputGroup label="State *">
-          <ListSelect list={states} selectMessage="Select a State" />
-        </InputGroup>
-        <InputGroup label="Zip Code *">
-          <Input type="text" name="postalCode" />
-        </InputGroup>
+    <Card>
+      <div class="flex items-center justify-between">
+        <Text color="gray">Patient Address</Text>
+        <Button>Save Address</Button>
       </div>
-    </form>
+      <form ref={form}>
+        <InputGroup label="Address Line 1 *" subLabel="Enter Street Number and Name">
+          <Input type="text" name="street1" />
+        </InputGroup>
+        <InputGroup label="Address Line 2" subLabel="Enter Apt, Unit, or Suite">
+          <Input type="text" name="street2" />
+        </InputGroup>
+        <InputGroup label="City *">
+          <Input type="text" name="city" />
+        </InputGroup>
+        <div class="grid grid-cols-2 gap-4">
+          <InputGroup label="State *">
+            <ListSelect list={states} selectMessage="Select a State" name="state" />
+          </InputGroup>
+          <InputGroup label="Zip Code *">
+            <Input type="text" name="postalCode" />
+          </InputGroup>
+        </div>
+      </form>
+    </Card>
   );
 }
