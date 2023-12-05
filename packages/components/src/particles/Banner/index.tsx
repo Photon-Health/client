3e722e776b5b5b676a893230c9d0caf40a8dab50
Paul Christophe <paul.christophe6@gmail.com>
2023-12-05 12:53:26 -0500

    init recent orders work

--- packages/components/src/particles/Banner/index.tsx
+++ packages/components/src/particles/Banner/index.tsx
@@ -7,7 +7,7 @@ type BannerProps = {
 
 export default function Banner(props: BannerProps) {
   return (
-    <div class="text-blue-600 flex items-center gap-2 bg-blue-50 py-3 px-3 sm:px-4 rounded-lg mb-4">
+    <div class="text-blue-600 flex items-center gap-2 bg-blue-50 py-3 px-3 sm:px-4 rounded-lg">
       <div class="flex-shrink-0">
         <Icon name="informationCircle" size="sm" />
       </div>
--- packages/elements/src/photon-multirx-form/index.tsx
+++ packages/elements/src/photon-multirx-form/index.tsx
@@ -19,7 +19,7 @@ import { createEffect, onMount, createSignal, Show, For } from 'solid-js';
 import type { FormError } from '../stores/form';
 import { createFormStore } from '../stores/form';
 import { usePhoton } from '../context';
-import { Spinner, Toaster, Button } from '@photonhealth/components';
+import { Spinner, Toaster, Button, RecentOrders } from '@photonhealth/components';
 import { Order, Prescription } from '@photonhealth/sdk/dist/types';
 import { AddPrescriptionCard } from './components/AddPrescriptionCard';
 import { PatientCard } from './components/PatientCard';
@@ -367,6 +367,9 @@ customElement(
                   store.patient?.value?.address || (store.patient?.value?.id && !props.enableOrder)
                 }
               >
+                <RecentOrders patientId={store.patient?.value?.id}>
+                  <RecentOrders.Card />
+                </RecentOrders>
                 <Show when={showForm() || isEditing()}>
                   <div ref={prescriptionRef}>
                     <AddPrescriptionCard
