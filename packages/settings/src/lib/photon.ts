c173b24355221fff9487187cd7dc8672c2609dc5
jknapp25 <joshknappp@gmail.com>
2023-12-06 15:34:14 -0800

    pine medical branding

--- packages/settings/src/lib/photon.ts
+++ packages/settings/src/lib/photon.ts
@@ -202,7 +202,6 @@ export const photon: {
   org_ZIksHxx59zFVc1Xw: {
     ...defaultSettings,
     logo: 'pine_medical_logo.svg',
-    accentColor: '#000000',
-    enableRxAndOrder: false
+    accentColor: '#000000'
   }
 };
