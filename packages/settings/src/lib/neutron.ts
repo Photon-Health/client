d45624e743ac3152a59366bc8d14ca756a8c1e0c
Sam Kotlove <sam@photon.health>
2023-12-28 14:57:17 -0500

    Add brightside branding to Neutron

--- packages/settings/src/lib/neutron.ts
+++ packages/settings/src/lib/neutron.ts
@@ -235,5 +235,11 @@ export const neutron: {
     logo: 'pine_medical_logo.svg',
     accentColor: '#000000',
     enableRxAndOrder: false
+  },
+  // Brightside
+  org_Dcq069P9AxLlv4l2: {
+    ...defaultSettings,
+    logo: 'brightside_health_logo.svg',
+    accentColor: '#2e4985'
   }
 };
