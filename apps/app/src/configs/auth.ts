1fe96c41900ea38e011fb7542c87c7597f782e46
Sam Kotlove <sam@photon.health>
2023-12-29 18:09:04 -0500

    Remove comment

--- apps/app/src/configs/auth.ts
+++ apps/app/src/configs/auth.ts
@@ -1,5 +1,3 @@
-// import { Auth0ProviderOptions } from '@auth0/auth0-react';
-
 export const auth0Config = {
   domain: process.env.REACT_APP_AUTH0_DOMAIN as string,
   clientId: process.env.REACT_APP_AUTH0_CLIENT_ID as string,
