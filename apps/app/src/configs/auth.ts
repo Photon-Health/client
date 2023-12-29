de5e59dbabeadd899d6f747f162e42222ac65ad7
Sam Kotlove <sam@photon.health>
2023-12-29 17:57:40 -0500

    Fix auth on browsers with ITP (i.e. safari)

--- apps/app/src/configs/auth.ts
+++ apps/app/src/configs/auth.ts
@@ -1,6 +1,6 @@
-import { Auth0ProviderOptions } from '@auth0/auth0-react';
+// import { Auth0ProviderOptions } from '@auth0/auth0-react';
 
-export const auth0Config: Auth0ProviderOptions = {
+export const auth0Config = {
   domain: process.env.REACT_APP_AUTH0_DOMAIN as string,
   clientId: process.env.REACT_APP_AUTH0_CLIENT_ID as string,
   audience: process.env.REACT_APP_AUTH0_AUDIENCE as string,
