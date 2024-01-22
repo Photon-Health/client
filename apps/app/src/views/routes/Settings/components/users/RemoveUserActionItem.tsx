9ffec5e17092b25d0e428939a984aee067992053
photonsFardad <120397057+photonsFardad@users.noreply.github.com>
2024-01-21 20:21:16 -0600

    RW updates

---
+++ apps/app/src/views/routes/Settings/components/users/RemoveUserActionItem.tsx
@@ -0,0 +1,97 @@
+// RemoveUserMenuItem.tsx
+import React from 'react';
+import { MenuItem, useToast, Text } from '@chakra-ui/react';
+import { FragmentType, graphql, useFragment } from 'apps/app/src/gql';
+import { FiTrash } from 'react-icons/fi';
+import { confirmWrapper } from '../../../../components/GuardDialog';
+import { useMutation } from '@apollo/client';
+import { usePhoton } from '@photonhealth/react';
+import { StyledToast } from 'apps/app/src/views/components/StyledToast';
+
+export const userFragment = graphql(/* GraphQL */ `
+  fragment UserFragment on User {
+    id
+    npi
+    phone
+    fax
+    email
+    address {
+      street1
+      street2
+      state
+      postalCode
+      country
+      city
+    }
+    name {
+      first
+      full
+      last
+      middle
+      title
+    }
+    roles {
+      description
+      id
+      name
+    }
+  }
+`);
+
+interface RemoveUserMenuItemProps {
+  user: FragmentType<typeof userFragment>;
+  onDelete: () => void;
+}
+
+const RemoveUserFromOrganizationMutation = graphql(/* GraphQL */ `
+  mutation UserItemActionRemoveUserFromOrganization($userId: ID!) {
+    removeUserFromOrganization(userId: $userId)
+  }
+`);
+
+const RemoveUserMenuItem: React.FC<RemoveUserMenuItemProps> = ({ user, onDelete }) => {
+  const toast = useToast();
+  const { clinicalClient } = usePhoton();
+  const userData = useFragment(userFragment, user);
+  const [removeUser, { loading: deleteLoading }] = useMutation(RemoveUserFromOrganizationMutation, {
+    client: clinicalClient,
+    refetchQueries: ['UsersListQuery']
+  });
+
+  const handleRemoveUser = async () => {
+    const decision = await confirmWrapper('Remove user?', {
+      description: <Text mb={2}>This will remove user from the organization.</Text>,
+      cancelText: 'Cancel',
+      confirmText: 'Delete',
+      colorScheme: 'red'
+    });
+
+    if (decision) {
+      await removeUser({
+        variables: {
+          userId: userData.id
+        }
+      });
+      onDelete();
+      toast({
+        position: 'top-right',
+        duration: 4000,
+        render: ({ onClose }) => (
+          <StyledToast onClose={onClose} type="success" description="Profile updated" />
+        )
+      });
+    }
+  };
+
+  return (
+    <MenuItem
+      icon={<FiTrash fontSize="1.3em" color="red" />}
+      disabled={deleteLoading}
+      onClick={handleRemoveUser}
+    >
+      Remove User
+    </MenuItem>
+  );
+};
+
+export default RemoveUserMenuItem;
--- apps/app/src/views/routes/Settings/components/users/UserItemActions.tsx
+++ apps/app/src/views/routes/Settings/components/users/UserItemActions.tsx
@@ -8,18 +8,12 @@ import {
   IconButton,
   Modal,
   ModalOverlay,
-  useDisclosure,
-  Box,
-  Icon,
-  Text,
-  useToast
+  useDisclosure
 } from '@chakra-ui/react';
-import { FiEdit, FiMoreVertical, FiTrash, FiCheckCircle, FiX } from 'react-icons/fi';
+import { FiEdit, FiMoreVertical } from 'react-icons/fi';
 import { EditRolesAction } from './EditRolesAction';
-import { FragmentType, graphql, useFragment } from 'apps/app/src/gql';
-import { confirmWrapper } from '../../../../components/GuardDialog';
-import { useMutation } from '@apollo/client';
-import { usePhoton } from '@photonhealth/react';
+import { FragmentType, graphql } from 'apps/app/src/gql';
+import RemoveUserMenuItem from './RemoveUserActionItem';
 
 export const userFragment = graphql(/* GraphQL */ `
   fragment UserFragment on User {
@@ -55,21 +49,9 @@ interface UserItemActionsProps {
   user: FragmentType<typeof userFragment>;
 }
 
-const RemoveUserFromOrganizationMutation = graphql(/* GraphQL */ `
-  mutation UserItemActionRemoveUserFromOrganization($userId: ID!) {
-    removeUserFromOrganization(userId: $userId)
-  }
-`);
-
 export const UserItemActions: React.FC<UserItemActionsProps> = ({ user }) => {
-  const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { clinicalClient } = usePhoton();
-  const [removeUser, { loading: deleteLoading }] = useMutation(RemoveUserFromOrganizationMutation, {
-    client: clinicalClient,
-    refetchQueries: ['UsersListQuery']
-  });
-  const userData = useFragment(userFragment, user);
+
   return (
     <HStack justifyContent="flex-end">
       <Menu autoSelect={false}>
@@ -83,54 +65,7 @@ export const UserItemActions: React.FC<UserItemActionsProps> = ({ user }) => {
           <MenuItem icon={<FiEdit fontSize="1.2em" />} onClick={onOpen}>
             Edit Roles
           </MenuItem>
-          <MenuItem
-            icon={<FiTrash fontSize="1.3em" color="red" />}
-            disabled={deleteLoading}
-            onClick={async () => {
-              const decision = await confirmWrapper('Remove user?', {
-                description: <Text mb={2}>This will remove user from organization.</Text>,
-                cancelText: 'Cancel',
-                confirmText: 'Delete',
-                colorScheme: 'red'
-              });
-              if (decision) {
-                removeUser({
-                  variables: {
-                    userId: userData.id
-                  }
-                });
-                toast({
-                  position: 'top',
-                  // TODO: Override default solid theme with this outline variant
-                  render: ({ onClose }) => (
-                    <Box
-                      color="gray.800"
-                      p={4}
-                      borderWidth="2px"
-                      borderRadius="md"
-                      bg="white"
-                      borderColor="green.500"
-                    >
-                      <HStack>
-                        <Icon as={FiCheckCircle} color="green.500" boxSize="5" />
-                        <Text flex="1">Invite Deleted</Text>
-                        <IconButton
-                          color="muted"
-                          icon={<FiX fontSize="1.25rem" />}
-                          variant="ghost"
-                          aria-label="close"
-                          title="Close"
-                          onClick={onClose}
-                        />
-                      </HStack>
-                    </Box>
-                  )
-                });
-              }
-            }}
-          >
-            Remove User
-          </MenuItem>
+          <RemoveUserMenuItem user={user} onDelete={onClose} />
         </MenuList>
       </Menu>
       <Modal isOpen={isOpen} onClose={onClose} size="xl">
