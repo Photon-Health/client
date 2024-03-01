2749442fead88beee5bcf966b25c6e7c20234c1f
Sam Kotlove <sam@photon.health>
2024-03-01 10:37:04 -0500

    Whitespace change to trigger build

--- apps/patient/src/index.tsx
+++ apps/patient/src/index.tsx
@@ -26,7 +26,6 @@ datadogRum.startSessionReplayRecording();
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
