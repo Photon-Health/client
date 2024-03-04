f94297743140c6ea7eb482ae18d6980656d914c8
photonsFardad <120397057+photonsFardad@users.noreply.github.com>
2024-03-04 14:47:45 -0600

    change photons phone number to link

--- apps/patient/src/views/Canceled.tsx
+++ apps/patient/src/views/Canceled.tsx
@@ -10,7 +10,7 @@ export const Canceled = () => {
         <Heading as="h4" size="md" textAlign="center">
           {t.orderCanceled}
         </Heading>
-        <Text textAlign="center">{t.questions}</Text>
+        <Text textAlign="center">{`${t.questionVerb} <a href="tel:${t.questionsPhoneNumber}">${t.questionsPhoneNumber}</a>.`}</Text>
       </VStack>
     </Center>
   );
