bcbdc311330ed81102eb956da1b23240349641ef
Paul Christophe <paul.christophe6@gmail.com>
2024-03-20 14:56:44 -0400

    show form

---
+++ packages/components/src/particles/Alert/Alert.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/html';
+import { ComponentProps } from 'solid-js';
+import { Alert } from './';
+
+type AlertStory = StoryObj<ComponentProps<typeof Alert>>;
+
+const meta: Meta<ComponentProps<typeof Alert>> = {
+  title: 'Alert',
+  component: Alert,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['success', 'info', 'warning', 'error']
+    },
+    header: {
+      control: 'text'
+    },
+    message: {
+      control: 'text'
+    }
+  }
+};
+
+export default meta;
+
+export const Default: AlertStory = {
+  args: {
+    type: 'warning',
+    header: 'Warning',
+    message: 'This is a warning alert.'
+  }
+};
+
+export const Success: AlertStory = {
+  args: {
+    type: 'success',
+    header: 'Success',
+    message: 'This is a success alert.'
+  }
+};
+
+export const Info: AlertStory = {
+  args: {
+    type: 'info',
+    header: 'Info',
+    message: 'This is an info alert.'
+  }
+};
+
+export const Error: AlertStory = {
+  args: {
+    type: 'error',
+    header: 'Error',
+    message: 'This is an error alert.'
+  }
+};
---
+++ packages/components/src/particles/Alert/Alert.tsx
@@ -0,0 +1,66 @@
+import { createMemo } from 'solid-js';
+import Icon from '../Icon';
+
+type AlertType = 'success' | 'info' | 'warning' | 'error';
+type AlertProps = {
+  type: AlertType;
+  header: string;
+  message: string;
+};
+
+export const Alert = (props: AlertProps) => {
+  const iconName = createMemo(() => {
+    switch (props.type) {
+      case 'success':
+        return 'checkCircle';
+      case 'info':
+        return 'informationCircle';
+      case 'warning':
+        return 'exclamationTriangle';
+      case 'error':
+        return 'xCircle';
+    }
+  });
+
+  const textColor = createMemo(() => {
+    switch (props.type) {
+      case 'success':
+        return 'text-green-800';
+      case 'info':
+        return 'text-blue-800';
+      case 'warning':
+        return 'text-yellow-800';
+      case 'error':
+        return 'text-red-800';
+    }
+  });
+
+  const bgColor = createMemo(() => {
+    switch (props.type) {
+      case 'success':
+        return 'bg-green-50';
+      case 'info':
+        return 'bg-blue-50';
+      case 'warning':
+        return 'bg-yellow-50';
+      case 'error':
+        return 'bg-red-50';
+    }
+  });
+
+  return (
+    <div class={`rounded-md ${bgColor()} p-4`}>
+      <div class="flex">
+        <div class="flex-shrink-0">
+          <Icon name={iconName()} class={textColor()} />
+        </div>
+        <div class="ml-3">
+          <h3 class={`text-base font-medium mt-0 ${textColor()}`}>{props.header}</h3>
+          <div class={`mt-2 ${textColor()}`}>
+            <p class="text-sm mb-0">{props.message}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
