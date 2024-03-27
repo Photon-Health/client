ad68690b89532b3503634be98eea46b40c042dd5
Paul Christophe <paul.christophe6@gmail.com>
2024-03-27 15:50:40 -0400

    Better Order Cancelled Page (#1069)
    * cancel
    
    * copy and component name
    
    * color

--- apps/patient/src/views/Canceled.tsx
+++ apps/patient/src/views/Canceled.tsx
@@ -1,17 +1,43 @@
-import { Center, Heading, Text, VStack } from '@chakra-ui/react';
-import { MdOutlineCancel } from 'react-icons/md';
-import { generatePhoneNumberLink, text as t } from '../utils/text';
+import { Box, Container, Heading, Text, VStack } from '@chakra-ui/react';
+import { Helmet } from 'react-helmet';
+import { PrescriptionsList } from '../components/PrescriptionsList';
+import { text as t } from '../utils/text';
+import { useOrderContext } from './Main';
 
 export const Canceled = () => {
+  const {
+    order: { patient }
+  } = useOrderContext();
+
   return (
-    <Center h="100vh">
-      <VStack>
-        <MdOutlineCancel size="2em" />
-        <Heading as="h4" size="md" textAlign="center">
-          {t.orderCanceled}
-        </Heading>
-        <Text textAlign="center">{generatePhoneNumberLink()}</Text>
-      </VStack>
-    </Center>
+    <Box>
+      <Helmet>
+        <title>{t.orderCanceled}</title>
+      </Helmet>
+
+      <Box bgColor="white" shadow="sm">
+        <Container>
+          <VStack spacing={2} align="start" py={4}>
+            <Heading as="h3" size="lg">
+              {t.orderCanceled}
+            </Heading>
+            <Box>
+              <Text display="inline">{t.reachOut}</Text>
+            </Box>
+          </VStack>
+        </Container>
+      </Box>
+
+      <Container>
+        <Text align="left" mt={4} fontSize="md" color="gray.500">
+          Patient{' '}
+          <Text as="span" color="gray.700">
+            {patient.name.full}
+          </Text>
+        </Text>
+      </Container>
+
+      <PrescriptionsList />
+    </Box>
   );
 };
