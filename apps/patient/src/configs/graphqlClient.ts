bf694b91df87484fd2f6f4cf86037e3ae899fe28
Michael Rochlin <rochlin@photon.health>
2024-04-10 18:46:50 -0400

    prettier

--- apps/patient/src/configs/graphqlClient.ts
+++ apps/patient/src/configs/graphqlClient.ts
@@ -1,11 +1,14 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const graphQLClient = new GraphQLClient((process.env as any).REACT_APP_GRAPHQL_API_ENDPOINT, {
-  jsonSerializer: {
-    parse: JSON.parse,
-    stringify: JSON.stringify
+export const graphQLClient = new GraphQLClient(
+  (process.env as any).REACT_APP_GRAPHQL_API_ENDPOINT,
+  {
+    jsonSerializer: {
+      parse: JSON.parse,
+      stringify: JSON.stringify
+    }
   }
-});
+);
 
 export const setAuthHeader = (token: string) => {
   graphQLClient.setHeaders({
