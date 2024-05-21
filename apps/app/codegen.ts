77826ea80e9d712a55765c71d5b2846405408f74
Michael Rochlin <rochlin@photon.health>
2024-05-21 13:58:23 -0400

    undo codegen

--- apps/app/codegen.ts
+++ apps/app/codegen.ts
@@ -1,8 +1,8 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://clinical-api.tau.health:8080/graphql',
-  // schema: 'http://clinical-api.boson.health/graphql',
+  // schema: 'http://clinical-api.tau.health:8080/graphql',
+  schema: 'http://clinical-api.boson.health/graphql',
   documents: ['apps/app/src/**/Settings/**/*.tsx', 'apps/app/src/views/components/TicketModal.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
