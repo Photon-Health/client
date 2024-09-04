62eee67e3ac655855c8762f9d719b1c159609ac0
jknapp25 <joshknappp@gmail.com>
2024-09-04 15:06:42 -0700

    duplicate style

--- packages/elements/src/photon-medication-dropdown/photon-medication-dropdown.tsx
+++ packages/elements/src/photon-medication-dropdown/photon-medication-dropdown.tsx
@@ -348,7 +348,7 @@ export const PhotonMedicationDropdown = <T extends { id: string }>(props: {
             ref={overlayRef}
             class="bg-white overflow-x-hidden overflow-y-auto relative"
             style={{
-              'max-height': '300px',
+              'max-height': '500px',
               'min-height': '37px',
               width: '100%',
               display: 'flex',
