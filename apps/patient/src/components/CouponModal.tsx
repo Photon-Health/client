28a8afc4ffa519b492c614e77d82ed8ebc6445e4
jknapp25 <joshknappp@gmail.com>
2024-09-12 15:30:59 -0700

    modal ui polishing

--- apps/patient/src/components/CouponModal.tsx
+++ apps/patient/src/components/CouponModal.tsx
@@ -1,37 +1,43 @@
 import {
-  Box,
   Button,
+  Heading,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
-  ModalOverlay
+  ModalOverlay,
+  Text,
+  VStack
 } from '@chakra-ui/react';
-import React from 'react';
+import { FC } from 'react';
 
 interface CouponModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-export const CouponModal: React.FC<CouponModalProps> = ({ isOpen, onClose }) => {
+export const CouponModal: FC<CouponModalProps> = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent py={3}>
         <ModalBody>
-          <Box mb={4}>
-            <strong>How to use this coupon</strong>
-            <p>Just show it to the pharmacist when you pick up your prescription.</p>
-          </Box>
-          <Box>
-            <strong>Can I use this coupon if I have health insurance?</strong>
-            <p>
+          <VStack mb={5} spacing={2} align="start">
+            <Heading as="h5" size="sm">
+              How to use this coupon
+            </Heading>
+            <Text>Just show it to the pharmacist when you pick up your prescription.</Text>
+          </VStack>
+          <VStack spacing={2} align="start">
+            <Heading as="h5" size="sm">
+              Can I use this coupon if I have health insurance?
+            </Heading>
+            <Text>
               The price on the coupon may be lower than your health insurance co-pay. It can be used
-              instead of your co-pay and does not apply to your deductible. Ask your pharmacist to
-              help you find the best possible price.
-            </p>
-          </Box>
+              instead of your co-pay and does not apply to your deductible.
+            </Text>
+            <Text>Ask your pharmacist to help you find the best possible price.</Text>
+          </VStack>
         </ModalBody>
         <ModalFooter>
           <Button variant="outline" colorScheme="blue" borderRadius="xl" onClick={onClose} w="full">
