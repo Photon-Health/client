15ae73e5ee07794b3875fee5694978d714745270
Sam Kotlove <smkotlove@gmail.com>
2024-09-13 17:24:55 -0400

    Fix stutter with mail order (#1371)
    * Fix stutter with mail order
    
    * Swap mail order logic to just hide mail orders if we're showing costco

--- apps/patient/src/views/Pharmacy.tsx
+++ apps/patient/src/views/Pharmacy.tsx
@@ -132,9 +132,8 @@ export const Pharmacy = () => {
   const hasTopRankedCostco = topRankedPharmacies.some((p) => p.name === 'Costco Pharmacy');
   const enableMailOrder =
     !isDemo &&
-    // If we're loading we shouldn't show pharmacies of mail order (we don't even know if we want to show until
-    // we see if there are top ranked costcos)
-    !loadingPharmacies &&
+    // If we're showing costco, we don't want to show mail order
+    !orgSettings.topRankedCostco &&
     !hasTopRankedCostco && // this means org is Sesame, we don't want to show Amazon and top ranked Costco at the same time
     orgSettings.mailOrderNavigate;
 
