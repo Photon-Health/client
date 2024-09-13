2ad86c3af29933243044a62ce8a8fea549d05b58
Sam <smkotlove@gmail.com>
2024-09-13 15:22:58 -0400

    Update costco logic to always show instead of just for GLP1 orders

--- apps/patient/src/views/Pharmacy.tsx
+++ apps/patient/src/views/Pharmacy.tsx
@@ -132,6 +132,9 @@ export const Pharmacy = () => {
   const hasTopRankedCostco = topRankedPharmacies.some((p) => p.name === 'Costco Pharmacy');
   const enableMailOrder =
     !isDemo &&
+    // If we're loading we shouldn't show pharmacies of mail order (we don't even know if we want to show until
+    // we see if there are top ranked costcos)
+    !loadingPharmacies &&
     !hasTopRankedCostco && // this means org is Sesame, we don't want to show Amazon and top ranked Costco at the same time
     orgSettings.mailOrderNavigate;
 
@@ -344,7 +347,7 @@ export const Pharmacy = () => {
         let topRankedPharmacies: EnrichedPharmacy[] = [];
 
         // check if top ranked costco is enabled and there are GLP treatments
-        if (enableTopRankedCostco && containsGLP) {
+        if (enableTopRankedCostco) {
           topRankedPharmacies = [
             ...(await getCostco({ latitude, longitude, enable24Hr, enableOpenNow })),
             ...topRankedPharmacies
