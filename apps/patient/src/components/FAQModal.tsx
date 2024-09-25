15f692612f8594dbd9206e17cb3f606bf1e1e0ce
jknapp25 <joshknappp@gmail.com>
2024-09-25 14:22:53 -0700

    spacing

--- apps/patient/src/components/FAQModal.tsx
+++ apps/patient/src/components/FAQModal.tsx
@@ -28,26 +28,28 @@ export const FAQModal = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => 
                 <FAQContents />
               </VStack>
 
-              <Heading as="h4" size="md">
-                Still need help?
-              </Heading>
-              <Card>
-                <VStack spacing={1} w="full">
-                  <Box>
-                    If you have other pharmacy related questions, we are available 24/7 for support.
-                    We typically respond within 30 minutes.
-                  </Box>
-                  <Button
-                    as="a"
-                    variant="outline"
-                    color="blue.500"
-                    href={`sms:${process.env.REACT_APP_TWILIO_SMS_NUMBER}`}
-                    w="full"
-                  >
-                    Message support
-                  </Button>
-                </VStack>
-              </Card>
+              <VStack w="full" alignItems="stretch" spacing={4}>
+                <Heading as="h4" size="md">
+                  Still need help?
+                </Heading>
+                <Card>
+                  <VStack spacing={1} w="full">
+                    <Box>
+                      If you have other pharmacy related questions, we are available 24/7 for
+                      support. We typically respond within 30 minutes.
+                    </Box>
+                    <Button
+                      as="a"
+                      variant="outline"
+                      color="blue.500"
+                      href={`sms:${process.env.REACT_APP_TWILIO_SMS_NUMBER}`}
+                      w="full"
+                    >
+                      Message support
+                    </Button>
+                  </VStack>
+                </Card>
+              </VStack>
             </VStack>
           </Container>
         </ModalBody>
