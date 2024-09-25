336f6bbe9622aaf1d33b28b1ddcf277064c06ca9
jknapp25 <joshknappp@gmail.com>
2024-09-25 09:39:07 -0700

    cleanup

--- apps/patient/src/components/CouponDetails.tsx
+++ apps/patient/src/components/CouponDetails.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, HStack, Icon, Text, VStack } from '@chakra-ui/react';
+import { Box, HStack, Icon, Text, VStack } from '@chakra-ui/react';
 import { FC, useState } from 'react';
 import { FiInfo } from 'react-icons/fi';
 import { CouponModal } from '../components';
@@ -16,45 +16,43 @@ export const CouponDetails: FC = () => {
   }
 
   return (
-    <Container>
+    <VStack w="full" spacing={4} mx={0}>
       <CouponModal isOpen={couponModalOpen} onClose={() => setCouponModalOpen(false)} />
-      <VStack w="full" spacing={4}>
-        <Text fontSize="4xl" fontWeight="700" py={0} lineHeight="1">
-          ${price}
+      <Text fontSize="4xl" fontWeight="700" py={0} lineHeight="1">
+        ${price}
+      </Text>
+      <Box bgColor="blue.50" w="full" textAlign="center" p={2} borderRadius="xl">
+        <Text fontWeight="semibold" fontSize="md">
+          {t.showThisCoupon}
         </Text>
-        <Box bgColor="blue.50" w="full" textAlign="center" p={2} borderRadius="xl">
-          <Text fontWeight="semibold" fontSize="md">
-            {t.showThisCoupon}
-          </Text>
-        </Box>
-        <HStack w="full" align="start">
-          <VStack w="30%" align="start">
-            <Text>{t.bin}</Text>
-            <Text>{t.pcn}</Text>
-            <Text>{t.group}</Text>
-            <Text>{t.memberId}</Text>
-          </VStack>
-          <VStack w="70%" align="start">
-            <Text as="b">{bin}</Text>
-            <Text as="b">{pcn}</Text>
-            <Text as="b">{group}</Text>
-            <Text as="b">{memberId}</Text>
-          </VStack>
-        </HStack>
-        <HStack color="blue.500">
-          <Icon as={FiInfo} />
-          <Text
-            as="u"
-            textUnderlineOffset="2px"
-            fontSize="sm"
-            fontWeight="semibold"
-            cursor="pointer"
-            onClick={() => setCouponModalOpen(true)}
-          >
-            {t.howToCoupon}
-          </Text>
-        </HStack>
-      </VStack>
-    </Container>
+      </Box>
+      <HStack w="full" align="start">
+        <VStack w="30%" align="start">
+          <Text>{t.bin}</Text>
+          <Text>{t.pcn}</Text>
+          <Text>{t.group}</Text>
+          <Text>{t.memberId}</Text>
+        </VStack>
+        <VStack w="70%" align="start">
+          <Text as="b">{bin}</Text>
+          <Text as="b">{pcn}</Text>
+          <Text as="b">{group}</Text>
+          <Text as="b">{memberId}</Text>
+        </VStack>
+      </HStack>
+      <HStack color="blue.500">
+        <Icon as={FiInfo} />
+        <Text
+          as="u"
+          textUnderlineOffset="2px"
+          fontSize="sm"
+          fontWeight="semibold"
+          cursor="pointer"
+          onClick={() => setCouponModalOpen(true)}
+        >
+          {t.howToCoupon}
+        </Text>
+      </HStack>
+    </VStack>
   );
 };
