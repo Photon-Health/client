02b43c5553ecc1752dfadf7d548eafdaa9d65ace
jknapp25 <joshknappp@gmail.com>
2024-09-26 16:19:24 -0700

    remove unused type

--- apps/patient/src/utils/models.ts
+++ apps/patient/src/utils/models.ts
@@ -9,14 +9,6 @@ import {
 type NotMaybe<T> = Exclude<T, null | undefined>;
 export type Order = NotMaybe<GetOrderQuery['order']>;
 export type Fill = Order['fills'][number];
-export interface DiscountCard {
-  id: string;
-  price: string;
-  bin: string;
-  pcn: string;
-  group: string;
-  memberId: string;
-}
 
 export type Fulfillment = Order['fulfillments'][number];
 export type Exception = Fulfillment['exceptions'][number];
