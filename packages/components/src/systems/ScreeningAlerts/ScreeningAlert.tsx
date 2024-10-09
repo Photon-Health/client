5ffe680ddfb6256ce336ea91ae14402cb7023ea7
Bill Killoran <bkilloran@bills-mbp.mynetworksettings.com>
2024-10-09 10:28:54 -0400

    fixup

--- packages/components/src/systems/ScreeningAlerts/ScreeningAlert.tsx
+++ packages/components/src/systems/ScreeningAlerts/ScreeningAlert.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 import Icon from '../../particles/Icon';
 import Text from '../../particles/Text';
 
@@ -9,44 +9,34 @@ export interface ScreeningAlertType {
   involvedEntityIds: string[];
 }
 
+/**
+ * Helper function to capitalize the severity
+ */
 const getSeverityText = (severity: string) => {
   return severity.charAt(0).toUpperCase() + severity.slice(1);
 };
 
-const getSeverityBorderColor = (severity: string) => {
+/**
+ * Helper function to get class names for styling based off of the severity
+ */
+const getColorBySeverity = (
+  severity: string
+): { icon: string; background: string; border: string } => {
   switch (severity) {
+    default:
     case 'minor':
-      return 'border-blue-200 ';
+      return { icon: 'text-blue-800 ', background: 'bg-blue-50', border: 'border-blue-200' };
     case 'moderate':
-      return 'border-yellow-200';
+      return { icon: 'text-yellow-800 ', background: 'bg-yellow-50', border: 'border-yellow-200' };
     case 'major':
-      return 'border-red-200';
+      return { icon: 'text-red-800 ', background: 'bg-red-50', border: 'border-yellow-200' };
   }
 };
 
-const getSeverityBackgroundColor = (severity: string) => {
-  switch (severity) {
-    case 'minor':
-      return 'bg-blue-50 ';
-    case 'moderate':
-      return 'bg-yellow-50';
-    case 'major':
-      return 'bg-red-50';
-  }
-};
-
-const getSeverityIconColor = (severity: string) => {
-  switch (severity) {
-    case 'minor':
-      return 'text-blue-800 ';
-    case 'moderate':
-      return 'text-yellow-800';
-    case 'major':
-      return 'text-red-800';
-  }
-};
-
-const getDescriptorOfOwningId = (owningId: string) => {
+/**
+ * Helper function to get the descriptor of the id to help contextualize the information
+ */
+const getDescriptorOfId = (owningId: string) => {
   const firstThreeChars = owningId.slice(0, 3);
 
   if (firstThreeChars === 'med') {
@@ -60,10 +50,21 @@ const getDescriptorOfOwningId = (owningId: string) => {
   return 'TBD';
 };
 
+/**
+ * Helper function to filter out entities that are the same as the owningId
+ * so we don't show unnecessary duplicate information that is present
+ * by nature of the association
+ */
 const filterOutOwningId = (owningId: string, involvedEntityIds: string[]): string[] => {
   return involvedEntityIds.filter((element) => element !== owningId);
 };
 
+/**
+ * This component is used to display a single alert for prescription screening.
+ *
+ * The `owningId` property is used to demonstrate this alert's relationship to an entity
+ * and will be used to filter out superfluous information otherwise highlighted by that relationship.
+ */
 export const ScreeningAlert = (props: { owningId: string; screeningAlert: ScreeningAlertType }) => {
   const [isExpanded, setIsExpanded] = createSignal<boolean>(false);
 
@@ -73,15 +74,15 @@ export const ScreeningAlert = (props: { owningId: string; screeningAlert: Screen
 
   return (
     <div
-      class={`border gap-4 rounded-lg ${getSeverityBorderColor(
-        props.screeningAlert.severity
-      )} ${getSeverityBackgroundColor(props.screeningAlert.severity)} `}
+      class={`border gap-4 rounded-lg ${getColorBySeverity(props.screeningAlert.severity).border} ${
+        getColorBySeverity(props.screeningAlert.severity).background
+      } `}
     >
       <div class="flex grid-flow-col justify-start">
         <div>
           <Icon
             name="exclamationCircle"
-            class={`mr-2 ${getSeverityIconColor(props.screeningAlert.severity)}`}
+            class={`mr-2 ${getColorBySeverity(props.screeningAlert.severity).icon}`}
           />
         </div>
         <div>
@@ -90,12 +91,24 @@ export const ScreeningAlert = (props: { owningId: string; screeningAlert: Screen
               {getSeverityText(props.screeningAlert.severity)}
             </Text>
             {' interaction with '}
-            <Text bold class="mb-2">
-              {filterOutOwningId(props.owningId, props.screeningAlert.involvedEntityIds).join(
-                ' and '
-              )}
-            </Text>
-            {` (${getDescriptorOfOwningId(props.owningId)})`}
+
+            <For each={filterOutOwningId(props.owningId, props.screeningAlert.involvedEntityIds)}>
+              {(entityId, index) => {
+                const descriptor = `(${getDescriptorOfId(entityId)})`;
+                return (
+                  <span>
+                    <Text bold class="mb-2">
+                      {entityId}
+                    </Text>
+                    {descriptor}
+                    {index() <
+                      filterOutOwningId(props.owningId, props.screeningAlert.involvedEntityIds)
+                        .length -
+                        1 && ' and '}
+                  </span>
+                );
+              }}
+            </For>
           </div>
           <div class={`${!isExpanded() ? 'hidden' : ''}`}>
             <Text size="sm">{props.screeningAlert.description}</Text>
--- packages/components/src/systems/ScreeningAlerts/ScreeningAlerts.tsx
+++ packages/components/src/systems/ScreeningAlerts/ScreeningAlerts.tsx
@@ -1,5 +1,11 @@
 import { ScreeningAlert, ScreeningAlertType } from './ScreeningAlert';
 
+/**
+ * This component represents multiple alerts for a given entity.
+ *
+ * The owningId property is used to demonstrate that this alert is attached to the provided ID
+ * and as a result filters out any information that is made superfluous by nature of the relationship.
+ */
 export const ScreeningAlerts = (props: {
   screeningAlerts: ScreeningAlertType[];
   owningId: string;
