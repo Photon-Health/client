75b43d4bf703fc1fc325fd8b94a0cc41ac87847c
Bill Killoran <bkilloran@bills-mbp.lan>
2025-02-10 18:00:43 -0500

    med history now renders data from new endpoint

--- packages/components/src/systems/PatientMedHistory/index.tsx
+++ packages/components/src/systems/PatientMedHistory/index.tsx
@@ -8,20 +8,21 @@ import generateString from '../../utils/generateString';
 import formatDate from '../../utils/formatDate';
 import Button from '../../particles/Button';
 import Card from '../../particles/Card';
-import { createQuery } from '../../utils/createQuery';
 import Icon from '../../particles/Icon';
+import { usePhoton } from '@photonhealth/react';
+import { useQuery } from '@apollo/client';
 
 const GET_PATIENT_MED_HISTORY = gql`
   query GetPatient($id: ID!) {
     patient(id: $id) {
       id
-      medicationHistory {
+      treatmentHistory {
         active
         prescription {
           id
           writtenAt
         }
-        medication {
+        treatment {
           id
           name
         }
@@ -45,15 +46,6 @@ type PatientMedHistoryProps = {
   newMedication?: Medication | SearchMedication;
 };
 
-type SelectedPatientMedication = Pick<PatientMedication, 'active' | 'medication' | 'prescription'>;
-
-type GetPatientResponse = {
-  patient: {
-    id: string;
-    medicationHistory: SelectedPatientMedication[];
-  };
-};
-
 const LoadingRowFallback = (props: { enableLinks: boolean }) => (
   <Table.Row>
     <Table.Cell>
@@ -72,23 +64,18 @@ const LoadingRowFallback = (props: { enableLinks: boolean }) => (
 
 export default function PatientMedHistory(props: PatientMedHistoryProps) {
   const client = usePhotonClient();
+  const { clinicalClient } = usePhoton();
   const [medHistory, setMedHistory] = createSignal<PatientMedication[] | undefined>(undefined);
   const [chronological, setChronological] = createSignal<boolean>(false);
 
   const baseURL = createMemo(() => `${client?.clinicalUrl}/prescriptions/`);
 
-  const queryOptions = createMemo(() => ({
-    variables: { id: props.patientId },
-    client: client!.apollo
-  }));
-
-  const patientMedHistory = createQuery<GetPatientResponse, { id: string }>(
-    GET_PATIENT_MED_HISTORY,
-    queryOptions
-  );
+  const { data, loading } = useQuery(GET_PATIENT_MED_HISTORY, {
+    client: clinicalClient
+  });
 
   createEffect(() => {
-    const medicationHistory = patientMedHistory()?.patient?.medicationHistory;
+    const medicationHistory = data()?.patient?.treatmentHistory;
     if (medicationHistory) {
       const sortedMedHistory = medicationHistory.slice().sort((a, b) => {
         const dateA = a?.prescription?.writtenAt
@@ -102,7 +89,7 @@ export default function PatientMedHistory(props: PatientMedHistoryProps) {
       });
       setMedHistory(sortedMedHistory);
     }
-    if (!patientMedHistory.loading && !medicationHistory) {
+    if (!loading && !medicationHistory) {
       setMedHistory([]);
     }
   });
