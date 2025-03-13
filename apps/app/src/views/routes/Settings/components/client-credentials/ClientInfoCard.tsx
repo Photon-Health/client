06450e946c6f98f98513727f07925f6957fc77ae
Morrison <nj.mrrsn@gmail.com>
2025-03-13 16:03:03 -0400

    Update URL Whitelist for SPA Clients in Developer Tab (#1635)
    * make readonly inputs look uneditable
    
    * codegen
    
    * use SPAClientEditForm on info card to update whitelisted urls
    
    * codegen
    
    * Edit readOnly styles in one place for Input values
    
    * allow empty input to clear urls, validate no query parameters, add hint
    
    * remove unnecessary diff

--- apps/app/src/views/routes/Settings/components/client-credentials/ClientInfoCard.tsx
+++ apps/app/src/views/routes/Settings/components/client-credentials/ClientInfoCard.tsx
@@ -5,6 +5,7 @@ import { AppNameView } from './AppNameView';
 import { ClientIdView } from './ClientIdView';
 import { ClientSecretView } from './ClientSecretView';
 import { RotateSecret } from './RotateSecret';
+import { SPAClientEditForm } from './SPAClientEditForm';
 import { FragmentType, graphql, useFragment } from 'apps/app/src/gql';
 
 const clientFragment = graphql(/* GraphQL */ `
@@ -13,8 +14,10 @@ const clientFragment = graphql(/* GraphQL */ `
     appType
     name
     secret
+    whiteListedUrls
   }
 `);
+
 interface ClientInfoCardProps {
   clientCreds: FragmentType<typeof clientFragment>;
 }
@@ -23,12 +26,17 @@ export const ClientInfoCard = (props: ClientInfoCardProps) => {
   const clientCreds = useFragment(clientFragment, props.clientCreds);
 
   return (
-    <Box as="form" {...props}>
+    <Box>
       <Stack spacing={6}>
         <AppDescriptionView appType={clientCreds.appType} />
         <AppNameView name={clientCreds.name ?? 'Client'} />
         <ClientIdView clientId={clientCreds.id} />
-        {clientCreds.appType !== 'spa' && (
+        {clientCreds.appType === 'spa' ? (
+          <SPAClientEditForm
+            clientId={clientCreds.id}
+            whiteListedUrls={clientCreds.whiteListedUrls}
+          />
+        ) : (
           <>
             {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
             <ClientSecretView clientSecret={clientCreds.secret!} clientId={clientCreds.id} />
