667b53bc0afb9248cf2e800873579c228912c2e1
Jake Stout <jake@photon.health>
2025-04-02 14:10:53 -0400

    Fix double scroll bar

--- packages/elements/src/photon-form-wrapper/photon-form-wrapper.tsx
+++ packages/elements/src/photon-form-wrapper/photon-form-wrapper.tsx
@@ -79,7 +79,7 @@ export const PhotonFormWrapper = (p: PhotonFormWrapperProps) => {
           </Show>
         </div>
       </header>
-      <div class="z-30 overflow-y-scroll w-full min-h-screen bg-[#F9FAFB] pt-28 xs:pt-28 lg:pt-20">
+      <div class="z-30 w-full min-h-screen bg-[#F9FAFB] pt-28 xs:pt-28 lg:pt-20">
         <div class="px-4 pb-40 md:pt-4 md:pb-52 md:px-4 w-full h-full sm:w-[600px] xs:mx-auto">
           {props.form}
         </div>
