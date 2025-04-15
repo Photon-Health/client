f4c5c47fbfb1335fa4e36d397410f5d6e7072049
Morrison <nj.mrrsn@gmail.com>
2025-04-15 15:17:28 -0400

    add neutron org id to settings gate

--- apps/app/src/views/routes/Settings/views/OrganizationTab.tsx
+++ apps/app/src/views/routes/Settings/views/OrganizationTab.tsx
@@ -4,13 +4,16 @@ import { Organization } from '../components/organization/Organization';
 import { OrganizationSettings } from '../components/organization/OrganizationSettings';
 import { usePhoton } from '@photonhealth/react';
 
+// Boson and Neutron test org ids
+const settingsTestOrgs = new Set(['org_KzSVZBQixLRkqj5d', 'org_kVS7AP4iuItESdMA']);
+
 export const OrganizationTab = () => {
   const { getOrganization } = usePhoton();
   const { organization } = getOrganization();
   return (
     <VStack spacing={5} align="left">
       <Organization />
-      {organization?.id === 'org_KzSVZBQixLRkqj5d' && <OrganizationSettings />}
+      {settingsTestOrgs.has(organization?.id) && <OrganizationSettings />}
     </VStack>
   );
 };
