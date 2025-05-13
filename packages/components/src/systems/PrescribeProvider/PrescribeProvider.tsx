70df2aed68099be75a871506a08db24003df0a2e
Jake Stout <jake@photon.health>
2025-05-13 15:02:44 -0400

    Fix double prefill prescriptions added (#1844)

--- packages/components/src/systems/PrescribeProvider/PrescribeProvider.tsx
+++ packages/components/src/systems/PrescribeProvider/PrescribeProvider.tsx
@@ -178,7 +178,6 @@ export const PrescribeProvider = (props: PrescribeProviderProps) => {
       setSelectedCoverageOption(undefined);
       setDidSelectOtherCoverageOption(false);
       setCoverageOptions([]);
-      setHasCreatedPrescriptions(false);
     }
   });
 
