9e1fb62b5d299d1e7e71eac25180bab2b8d06924
Jake Stout <jake@photon.health>
2025-06-26 14:09:20 -0400

    Setup patient app tests (#1937)
    * Setup patient app tests
    
    * Setup patient pharmacy tests
    
    * Incorporate nick and bill mob session feedback
    
    * Cleanup tests
    
    * Cleanup tests
    
    * fix flattened fills
    
    * Add patient test to PR check
    
    * Attempt number input test fix
    
    * Simplify vitest config for components
    
    * Fix patient test failure

---
+++ apps/patient/vite.config.mts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules', 'dist', '.git', '.cache']
+  }
+});
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -25,21 +25,6 @@ export default defineConfig({
   },
   test: {
     environment: 'jsdom',
-    globals: true,
-    transformMode: {
-      web: [/\.[jt]sx?$/]
-    },
-    // vitest can't seem to find jest-dom, it looks
-    // locally but not at the root node_modules
-    setupFiles: ['../../node_modules/@testing-library/jest-dom/extend-expect.js'],
-    // solid needs to be inline to work around
-    // a resolution issue in vitest:
-    deps: {
-      inline: [/solid-js/]
-    },
-    // if you have few tests, try commenting one
-    // or both out to improve performance:
-    threads: false,
-    isolate: false
+    globals: true
   }
 });
