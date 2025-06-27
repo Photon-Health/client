bac7cc0a54c9164c0e040e5dadc398a12b1ef2dd
Jake Stout <jake@photon.health>
2025-06-27 17:33:57 -0400

    update number format tests

--- apps/patient/src/utils/formatters.test.ts
+++ apps/patient/src/utils/formatters.test.ts
@@ -1,6 +1,9 @@
 import { formatPrice } from './formatters';
 
-test('formatPrice', () => {
+test('formatPrice shows 2 decimal places when there are fractionals', () => {
   expect(formatPrice(100.1)).toEqual('100.10');
+});
+
+test('formatPrice hides decimal places for whole numbers', () => {
   expect(formatPrice(100)).toEqual('100');
 });
