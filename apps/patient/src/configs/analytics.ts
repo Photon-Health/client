46e7eb997b4403f701c089c9817131823feec830
Paul Christophe <paul.christophe6@gmail.com>
2025-07-30 12:15:26 -0400

    Barebones Patient Analytics (via Rudderstack) (#1969)
    * init
    
    * update
    
    * announce page
    
    * pages
    
    * oh, need that
    
    * blocking test
    
    * test

---
+++ apps/patient/src/configs/analytics.ts
@@ -0,0 +1,40 @@
+import { ApiObject, RudderAnalytics } from '@rudderstack/analytics-js';
+
+const RUDDERSTACK_WRITE_KEY = process.env.REACT_APP_RUDDERSTACK_WRITE_KEY;
+const RUDDERSTACK_DATA_PLANE_URL = process.env.REACT_APP_RUDDERSTACK_DATA_PLANE_URL;
+const ENVIRONMENT = process.env.REACT_APP_ENV_NAME || 'development';
+
+export class PatientAnalytics {
+  private rudderanalytics?: RudderAnalytics;
+  private environment = 'development';
+
+  constructor() {
+    if (!RUDDERSTACK_WRITE_KEY || !RUDDERSTACK_DATA_PLANE_URL) {
+      console.error('RudderStack write key and data plane URL are required');
+      return;
+    }
+
+    this.rudderanalytics = new RudderAnalytics();
+    this.rudderanalytics.load(RUDDERSTACK_WRITE_KEY || '', RUDDERSTACK_DATA_PLANE_URL || '');
+    this.environment = ENVIRONMENT;
+  }
+
+  page(category: string, name?: string, properties: ApiObject = {}) {
+    if (!this.rudderanalytics) {
+      return;
+    }
+
+    const pageProperties = {
+      environment: this.environment,
+      ...properties
+    };
+
+    if (name) {
+      this.rudderanalytics.page(category, name, pageProperties);
+    } else {
+      this.rudderanalytics.page(category, pageProperties);
+    }
+  }
+}
+
+export const patientAnalytics = new PatientAnalytics();
--- apps/patient/src/views/Status.tsx
+++ apps/patient/src/views/Status.tsx
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 import { useEffect, useMemo, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { FiNavigation, FiPhoneCall, FiRefreshCcw } from 'react-icons/fi';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { triggerDemoNotification } from '../api';
 import { Coupons, DemoCtaModal, PharmacyInfo, PoweredBy } from '../components';
 import { Card } from '../components/Card';
@@ -22,11 +22,12 @@ import { InsuranceAlert } from '../components/InsuranceAlert';
 import { text as t } from '../utils/text';
 import { useOrderContext } from './Main';
 import { formatAddress } from '../utils/formatters';
+import { patientAnalytics } from '../configs/analytics';
 
 export const Status = () => {
   const navigate = useNavigate();
   const { order, setOrder, isDemo, setFaqModalIsOpen } = useOrderContext();
-
+  const location = useLocation();
   const { enablePatientRerouting } = order?.organization?.settings?.patientUx ?? {};
 
   const [searchParams] = useSearchParams();
@@ -52,6 +53,10 @@ export const Status = () => {
     if (!pharmacy?.phone) return;
     window.location.href = `tel:${pharmacy.phone}`;
   };
+
+  useEffect(() => {
+    patientAnalytics.page(location.pathname, 'Order Status');
+  }, [location.pathname]);
 
   useEffect(() => {
     if (!phone || !pharmacy || !order) {
