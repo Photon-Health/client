e11f3dc8f7ce849ec07af160d9bcb71d2e8fe972
Bill Killoran <bkilloran@bills-mbp.lan>
2025-08-15 13:55:41 -0400

    fixup

--- apps/patient/src/components/order-details/OrderDetailsModal.tsx
+++ apps/patient/src/components/order-details/OrderDetailsModal.tsx
@@ -15,7 +15,6 @@ import {
 import dayjs from 'dayjs';
 import { ReactNode } from 'react';
 import { MdOutlineLocalPharmacy } from 'react-icons/md';
-import { patientAnalytics } from '../../configs/analytics';
 
 export interface PrescriptionData {
   rxName: string;
@@ -72,10 +71,6 @@ const defaultIcon = (
 
 export const OrderDetailsModal = (props: OrderDetailsProps & OrderDetailsModalProps) => {
   const handleClose = () => {
-    patientAnalytics.track('Close Order Details Modal', {
-      pharmacyName: props.pharmacyName,
-      prescriptionCount: props.prescriptions.length
-    });
     props.onClose();
   };
 
