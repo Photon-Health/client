e25cfbd34e63239d0d9d7bbe2ba5e274b2e09d04
Jake Stout <jake@photon.health>
2025-08-19 17:09:55 -0400

    Move public to top

--- apps/patient/src/utils/shouldShowPriceToggle.tsx
+++ apps/patient/src/utils/shouldShowPriceToggle.tsx
@@ -3,16 +3,16 @@ import { Order } from './models';
 import { isGLP } from './isGLP';
 
 // note: prices are only for single-rx right now
-function shouldHideGlp1Prices(flattenedFills: FillWithCount[], organizationId: string) {
-  const isOrgThatHidesGlp1Prices = orgsThatHideGlp1Prices.includes(organizationId);
-  const hasGlp1 = flattenedFills.some((fill) => isGLP(fill.treatment.name));
-  return isOrgThatHidesGlp1Prices && hasGlp1;
-}
-
 export function shouldShowPriceToggle(flattenedFills: FillWithCount[], order: Order): boolean {
   const hideGlp1Prices = shouldHideGlp1Prices(flattenedFills, order.organization.id);
   const orderIsMultiRx = flattenedFills.length > 1;
   return (!hideGlp1Prices && !orderIsMultiRx) ?? false;
+}
+
+function shouldHideGlp1Prices(flattenedFills: FillWithCount[], organizationId: string) {
+  const isOrgThatHidesGlp1Prices = orgsThatHideGlp1Prices.includes(organizationId);
+  const hasGlp1 = flattenedFills.some((fill) => isGLP(fill.treatment.name));
+  return isOrgThatHidesGlp1Prices && hasGlp1;
 }
 
 const orgsThatHideGlp1Prices = ['org_QFoulY6Ornx7dMdw', 'org_hidesGlp1Prices'];
