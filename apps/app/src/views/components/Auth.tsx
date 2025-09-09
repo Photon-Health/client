a7c856b2db05e60b4ba63daa5a280e97eaec14a2
Jake Stout <jake@photon.health>
2025-09-09 11:15:04 -0400

    Remove unreachable logout button

--- apps/app/src/views/components/Auth.tsx
+++ apps/app/src/views/components/Auth.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import { usePhoton } from '@photonhealth/react';
 
 interface AuthProps {
@@ -6,24 +6,9 @@ interface AuthProps {
 }
 
 export const Auth = ({ returnTo = '/' }: AuthProps) => {
-  const { isLoading, isAuthenticated, login, logout } = usePhoton();
+  const { isLoading, login } = usePhoton();
 
   if (isLoading) return <Button isLoading loadingText="Loading" colorScheme="gray" />;
-
-  if (isAuthenticated)
-    return (
-      <Stack direction="row" spacing={4}>
-        <Button
-          colorScheme="brand"
-          onClick={() => {
-            localStorage.removeItem('previouslyAuthed');
-            logout({ returnTo: window.location.origin });
-          }}
-        >
-          Log out
-        </Button>
-      </Stack>
-    );
 
   return (
     <Button colorScheme="blue" onClick={() => login({ appState: { returnTo } })}>
