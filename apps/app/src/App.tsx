e0d1a289fcd1bb36e84ea43e461270f361c00021
Jake Stout <jake@photon.health>
2025-10-28 18:24:18 -0400

    SSO page with returnTo post auth (#2079)
    * setup allowlist for returnTo

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -37,7 +37,12 @@ const client = new PhotonClient({
 });
 
 const onRedirectCallback = (appState?: AppState) => {
-  window.location.replace(appState?.returnTo || window.location.pathname);
+  let returnTo = localStorage.getItem('authReturnTo');
+  localStorage.removeItem('authReturnTo');
+  if (!returnTo) {
+    returnTo = appState?.returnTo || window.location.pathname;
+  }
+  window.location.replace(returnTo);
 };
 
 export const App = () => {
