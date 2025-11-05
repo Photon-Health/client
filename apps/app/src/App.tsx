524431d4cfa84606d6ef6cc4ac358b32696c3d49
Jake Stout <jake@photon.health>
2025-11-05 14:05:21 -0500

    remove unnecessary params

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -38,11 +38,7 @@ const client = new PhotonClient({
 });
 
 const onRedirectCallback = (appState?: AppState) => {
-  let returnTo = localStorage.getItem('authReturnTo');
-  localStorage.removeItem('authReturnTo');
-  if (!returnTo) {
-    returnTo = appState?.returnTo || window.location.pathname;
-  }
+  const returnTo = appState?.returnTo || window.location.pathname;
   window.location.replace(returnTo);
 };
 
