0f6f28b0e9aa93caf17caa2b2833469942b2845f
Jake Stout <jake@photon.health>
2025-11-06 11:27:24 -0500

    add name, email, address input fields

--- apps/app/src/views/routes/SelfSignup/views/SelfSignupPage.tsx
+++ apps/app/src/views/routes/SelfSignup/views/SelfSignupPage.tsx
@@ -3,9 +3,21 @@ import { useSearchParams } from 'react-router-dom';
 import { auth0Config } from '../../../../configs/auth';
 import { useState } from 'react';
 
+interface SignupFormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  npi: string;
+  street1: string;
+  street2: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
 export const SelfSignupPage = () => {
   const [searchParams] = useSearchParams();
-  const [npiInput, setNpiInput] = useState('');
+
   const state = searchParams.get('state') ?? undefined;
   const sessionToken = searchParams.get('session_token') ?? undefined;
 
@@ -15,18 +27,68 @@ export const SelfSignupPage = () => {
 
   const { firstName, lastName, email } = extractTokenData(sessionToken);
 
+  const [firstNameInput, setFirstNameInput] = useState(firstName || '');
+  const [lastNameInput, setLastNameInput] = useState(lastName || '');
+  const [emailInput, setEmailInput] = useState(email || '');
+  const [npiInput, setNpiInput] = useState('');
+
+  const [street1Input, setStreet1Input] = useState('');
+  const [street2Input, setStreet2Input] = useState('');
+  const [zipInput, setZipInput] = useState('');
+  const [cityInput, setCityInput] = useState('');
+  const [stateInput, setStateInput] = useState('');
+
   const onAcceptClick = async () => {
-    window.location.href = `https://${auth0Config.domain}/continue?state=${state}&did_accept_tos=true&npi=${npiInput}`;
+    const queryParams = buildSignupContinueParams(state, {
+      firstName: firstNameInput,
+      lastName: lastNameInput,
+      email: emailInput,
+      npi: npiInput,
+      street1: street1Input,
+      street2: street2Input,
+      city: cityInput,
+      state: stateInput,
+      zip: zipInput
+    });
+    window.location.href = `https://${auth0Config.domain}/continue?${queryParams}`;
   };
 
   return (
     <Container maxW="md" py={{ base: '12' }}>
       <Stack spacing="8" textAlign="center">
         <h1>Terms of Service</h1>
-        <p>
-          {firstName} {lastName}
-        </p>
-        <p>{email}</p>
+
+        <FormControl>
+          <FormLabel htmlFor="firstName">First Name</FormLabel>
+          <Input
+            id="firstName"
+            value={firstNameInput}
+            onChange={(e) => setFirstNameInput(e.target.value)}
+            placeholder="First Name"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="lastName">Last Name</FormLabel>
+          <Input
+            id="lastName"
+            value={lastNameInput}
+            onChange={(e) => setLastNameInput(e.target.value)}
+            placeholder="Last Name"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input
+            id="email"
+            type="email"
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+            placeholder="Email Address"
+          />
+        </FormControl>
+
         <FormControl>
           <FormLabel htmlFor="npi">NPI</FormLabel>
           <Input
@@ -38,6 +100,59 @@ export const SelfSignupPage = () => {
             minLength={10}
           />
         </FormControl>
+
+        <p>Address</p>
+
+        <FormControl>
+          <FormLabel htmlFor="street1">Street 1</FormLabel>
+          <Input
+            id="street1"
+            value={street1Input}
+            onChange={(e) => setStreet1Input(e.target.value)}
+            placeholder="Street 1"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="street2">Street 2</FormLabel>
+          <Input
+            id="street2"
+            value={street2Input}
+            onChange={(e) => setStreet2Input(e.target.value)}
+            placeholder="Street 2 (optional)"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="city">City</FormLabel>
+          <Input
+            id="city"
+            value={cityInput}
+            onChange={(e) => setCityInput(e.target.value)}
+            placeholder="City"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="state">State</FormLabel>
+          <Input
+            id="state"
+            value={stateInput}
+            onChange={(e) => setStateInput(e.target.value)}
+            placeholder="State"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="zip">ZIP Code</FormLabel>
+          <Input
+            id="zip"
+            value={zipInput}
+            onChange={(e) => setZipInput(e.target.value)}
+            placeholder="ZIP Code"
+          />
+        </FormControl>
+
         <Button onClick={onAcceptClick}>Accept</Button>
       </Stack>
     </Container>
@@ -55,3 +170,21 @@ function extractTokenData(tosSessionToken?: string) {
   const email: string = decodedPayload.email;
   return { firstName, lastName, email };
 }
+
+const buildSignupContinueParams = (state: string, formData: SignupFormData): string => {
+  const params = new URLSearchParams({
+    state,
+    did_accept_tos: 'true',
+    first_name: formData.firstName,
+    last_name: formData.lastName,
+    email: formData.email,
+    npi: formData.npi,
+    street1: formData.street1,
+    street2: formData.street2,
+    city: formData.city,
+    state_address: formData.state,
+    zip: formData.zip
+  });
+
+  return params.toString();
+};
