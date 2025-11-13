9987616fa0a297001cdae884e5524a74d167ad17
Morrison <nj.mrrsn@gmail.com>
2025-11-13 17:32:25 -0500

    Hide the mail order dropdown when there are no results, opt to client side name filtering rather than server side name filtering for now

--- packages/components/src/systems/PharmacySearch/MailOrderPharmacySearch.tsx
+++ packages/components/src/systems/PharmacySearch/MailOrderPharmacySearch.tsx
@@ -2,7 +2,7 @@ import { Address, Pharmacy } from '@photonhealth/sdk/dist/types';
 import { ListPharmaciesQuery } from '../../fetch';
 import { usePhotonClient } from '../SDKProvider';
 import { PharmacyOption, PharmacySearchInput } from './PharmacySearch';
-import { createEffect, createMemo, createSignal, onCleanup } from 'solid-js';
+import { createEffect, createMemo, createSignal } from 'solid-js';
 
 type PharmacyListResult = Pick<Pharmacy, 'id' | 'name'> & {
   address: Pick<Address, 'street1' | 'city' | 'state'>;
@@ -16,26 +16,41 @@ type MailOrderPharmacySearchProps = {
 export function MailOrderPharmacySearch(props: MailOrderPharmacySearchProps) {
   const client = usePhotonClient();
   const [loading, setLoading] = createSignal<boolean>(false);
-  const [nameSearch, setNameSearch] = createSignal<string>();
-  const [debouncedNameSearch, setDebouncedNameSearch] = createSignal<string>();
+  const [nameSearch, setNameSearch] = createSignal<string>('');
   const [pharmacies, setPharmacies] = createSignal<PharmacyListResult[]>();
 
-  const pharmacyOptions = createMemo(() =>
-    pharmacies()?.map((option) => ({ ...option, isPreferred: false, isPrevious: false }))
-  );
+  const hidden = createMemo(() => {
+    // hide the combobox if there are no non-integrated mail orders in the env
+    // our temporary approach to feature flagging
+    const pharmacyResults = pharmacies();
+    return pharmacyResults && pharmacyResults.length === 0;
+  });
+
+  const pharmacyOptions = createMemo(() => {
+    // just doing client side filtering for now, since there's only like 8 viable pharmacies
+    const search = nameSearch();
+    const nameRegex = search ? new RegExp(search, 'i') : null;
+
+    const byName = (pharmacy: PharmacyListResult) => !nameRegex || nameRegex.test(pharmacy.name);
+    const toOption = (pharmacy: PharmacyListResult) => ({
+      ...pharmacy,
+      isPreferred: false,
+      isPrevious: false
+    });
+
+    return pharmacies()?.filter(byName).map(toOption);
+  });
 
   createEffect(() => {
     // effect hook for loading the pharmacies
     async function loadPharmacies() {
       setLoading(true);
       try {
-        const name = debouncedNameSearch() || undefined;
         const { data } = await client.apolloClinical.query({
           query: ListPharmaciesQuery,
           variables: {
             offset: 0,
             limit: 40,
-            name,
             fulfillmentType: 'MAIL_ORDER',
             integrated: false
           }
@@ -51,27 +66,28 @@ export function MailOrderPharmacySearch(props: MailOrderPharmacySearchProps) {
   });
 
   createEffect(() => {
-    // debounce the search query usage
-    const name = nameSearch();
-    const timeoutId = setTimeout(() => {
-      setDebouncedNameSearch(name);
-    }, 600);
-
-    onCleanup(() => clearTimeout(timeoutId));
+    // if selected value is cleared, then clear the name search
+    if (!props.selected) {
+      setNameSearch('');
+    }
   });
 
   return (
-    <PharmacySearchInput
-      label={
-        <div class="mb-2">
-          <label>Search home delivery options</label>
-        </div>
-      }
-      options={pharmacyOptions()}
-      onSearch={setNameSearch}
-      value={props.selected}
-      setValue={props.selectPharmacy}
-      loading={loading()}
-    />
+    <>
+      {hidden() ? null : (
+        <PharmacySearchInput
+          label={
+            <div class="mb-2">
+              <label>Search home delivery options</label>
+            </div>
+          }
+          options={pharmacyOptions()}
+          onSearch={setNameSearch}
+          value={props.selected}
+          setValue={props.selectPharmacy}
+          loading={loading()}
+        />
+      )}
+    </>
   );
 }
