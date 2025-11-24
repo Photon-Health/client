276fb21924a0a5f88ed5c24dacbda6382e31dc06
Jake Stout <jake@photon.health>
2025-11-24 14:20:42 -0500

    fix e2e test

--- apps/app/e2e/create_prescription.spec.ts
+++ apps/app/e2e/create_prescription.spec.ts
@@ -14,6 +14,11 @@ test('user can login and create patient', async ({ page }) => {
   await page.getByLabel('Mobile Number').fill('8886543210');
   await page.getByLabel('Sex at Birth').click();
   await page.getByRole('menuitem', { name: 'MALE', exact: true }).click();
+  await page.getByLabel('Street 1').fill('1 E2E Test St.');
+  await page.getByLabel('City').fill('e2e-test-city');
+  await page.getByLabel('State').click();
+  await page.getByRole('menuitem', { name: 'AL', exact: true }).click();
+  await page.getByLabel('Zip Code').fill('12345');
 
   await page.getByRole('button', { name: 'Save', exact: true }).click();
 
