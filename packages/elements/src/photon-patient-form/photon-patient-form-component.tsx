174d4ca89ca61faf062992dbde48a2137c775e0d
Morrison <nj.mrrsn@gmail.com>
2025-11-25 18:02:58 -0500

    put sex input before gender input in patient form

--- packages/elements/src/photon-patient-form/photon-patient-form-component.tsx
+++ packages/elements/src/photon-patient-form/photon-patient-form-component.tsx
@@ -288,27 +288,6 @@ const PatientForm = (props: { patientId: string }) => {
                 </div>
                 <div class="flex flex-col xs:flex-row justify-between xs:gap-4">
                   <div class="flex-grow w-full xs:min-w-[40%]">
-                    <photon-gender-input
-                      label="Gender"
-                      required="false"
-                      help-text={store['gender']?.error}
-                      invalid={store['gender']?.error !== undefined}
-                      on:photon-gender-selected={(e: any) => {
-                        actions.updateFormValue({
-                          key: 'gender',
-                          value: e.detail.gender
-                        });
-                      }}
-                      on:photon-gender-deselected={() => {
-                        actions.updateFormValue({
-                          key: 'gender',
-                          value: undefined
-                        });
-                      }}
-                      selected={pStore.selectedPatient.data?.gender}
-                    />
-                  </div>
-                  <div class="flex-grow w-full xs:min-w-[40%]">
                     <photon-sex-input
                       label="Sex at Birth"
                       required="true"
@@ -327,6 +306,27 @@ const PatientForm = (props: { patientId: string }) => {
                         });
                       }}
                       selected={pStore.selectedPatient.data?.sex}
+                    />
+                  </div>
+                  <div class="flex-grow w-full xs:min-w-[40%]">
+                    <photon-gender-input
+                      label="Gender"
+                      required="false"
+                      help-text={store['gender']?.error}
+                      invalid={store['gender']?.error !== undefined}
+                      on:photon-gender-selected={(e: any) => {
+                        actions.updateFormValue({
+                          key: 'gender',
+                          value: e.detail.gender
+                        });
+                      }}
+                      on:photon-gender-deselected={() => {
+                        actions.updateFormValue({
+                          key: 'gender',
+                          value: undefined
+                        });
+                      }}
+                      selected={pStore.selectedPatient.data?.gender}
                     />
                   </div>
                 </div>
