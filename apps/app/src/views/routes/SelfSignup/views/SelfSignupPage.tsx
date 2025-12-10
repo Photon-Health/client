8c1b2ac590f662f2fc30fe116e24ab9f18b70dde
Morrison <nj.mrrsn@gmail.com>
2025-12-10 13:45:56 -0500

    Copy adjustments

--- apps/app/src/views/routes/SelfSignup/views/SelfSignupPage.tsx
+++ apps/app/src/views/routes/SelfSignup/views/SelfSignupPage.tsx
@@ -364,26 +364,29 @@ export const SelfSignupPage = () => {
           </Formik>
         </Container>
       ) : (
-        <Container maxW="lg" textAlign="center" marginY="8">
+        <Container maxW="lg" marginY="8">
           <Alert status="error">
             <AlertIcon />
             <AlertDescription fontSize="sm">
-              You must be a verified prescriber to signup.{' '}
-              {supportEmail && (
-                <span>
-                  Email{' '}
-                  <Link
-                    href={`mailto:${supportEmail}`}
-                    textDecoration="underline"
-                    _before={{ display: 'none' }}
-                  >
-                    {supportEmail}
-                  </Link>{' '}
-                  for assistance
-                </span>
-              )}
+              Your identity or prescribing credentials haven’t been verified, so you can’t access
+              this page
             </AlertDescription>
           </Alert>
+          {supportEmail && (
+            <Text fontSize="sm" marginY="4" textAlign="center">
+              <span>
+                Please reach out to{' '}
+                <Link
+                  href={`mailto:${supportEmail}`}
+                  textDecoration="underline"
+                  _before={{ display: 'none' }}
+                >
+                  {supportEmail}
+                </Link>{' '}
+                if you believe this is an error or need help completing verification
+              </span>
+            </Text>
+          )}
         </Container>
       )}
     </>
