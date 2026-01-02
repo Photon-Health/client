ed9f798be50bd1fb8811f0963b43d86a6002c68c
Midori Yang <midori@photon.health>
2026-01-02 16:30:14 -0500

    rm effectiveDate fix

--- apps/app/src/views/components/FillsSelect.tsx
+++ apps/app/src/views/components/FillsSelect.tsx
@@ -34,7 +34,7 @@ export const FillsSelect = ({ initialFills = [], patientId, name }: FillsSelectP
   const { getPrescriptions } = usePhoton();
 
   const mapOption = (script: any) => {
-    const effective = script.effectiveDate ? formatDate(script.effectiveDate) : 'N/A';
+    const effective = formatDate(script.effectiveDate);
     const expires = formatDate(script.expirationDate);
     return {
       value: {
