51ca96bd4dd6d0a97ffefcb480b00c99588fa455
Midori Yang <midori@photon.health>
2026-01-08 12:13:52 -0500

    fix test again

--- apps/app/e2e/create_prescription.spec.ts
+++ apps/app/e2e/create_prescription.spec.ts
@@ -8,17 +8,17 @@ test('user can login and create patient', async ({ page }) => {
 
   const patientNumber = getRandomInt(0, 100_000_000);
 
-  await page.getByLabel('First Name').fill('Jimbob');
-  await page.getByLabel('Last Name').fill(`McTesterson_${patientNumber}`);
-  await page.getByLabel('Date of Birth').fill('1980-12-31');
-  await page.getByLabel('Mobile Number').fill('8886543210');
-  await page.getByLabel('Sex at Birth').click();
+  await page.getByLabel('First name').fill('Jimbob');
+  await page.getByLabel('Last name').fill(`McTesterson_${patientNumber}`);
+  await page.getByLabel('Date of birth').fill('1980-12-31');
+  await page.getByLabel('Mobile number').fill('8886543210');
+  await page.getByLabel('Sex at birth').click();
   await page.getByRole('menuitem', { name: 'Male', exact: true }).click();
   await page.getByLabel('Street 1').fill('1 E2E Test St.');
   await page.getByLabel('City').fill('e2e-test-city');
   await page.getByLabel('State').click();
   await page.getByRole('menuitem', { name: 'AL', exact: true }).click();
-  await page.getByLabel('Zip Code').fill('12345');
+  await page.getByLabel('Zip code').fill('12345');
 
   await page.getByRole('button', { name: 'Create', exact: true }).click();
 
