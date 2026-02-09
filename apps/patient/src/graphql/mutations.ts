837b17358e9bab103caf5ee645bd0fd370343b7e
Midori Yang <midori@photon.health>
2026-02-09 12:24:36 -0500

    update import

--- apps/patient/src/graphql/mutations.ts
+++ apps/patient/src/graphql/mutations.ts
@@ -1,4 +1,4 @@
-import { gql } from 'graphql-request';
+import { gql } from '@apollo/client';
 
 export const MARK_ORDER_AS_PICKED_UP = gql`
   mutation MarkOrderAsPickedUp($markOrderAsPickedUpId: ID!) {
