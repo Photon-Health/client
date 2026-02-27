51969b96ca858d121bf101a5ed5bef10d0f9c8bc
Midori Yang <midori@photon.health>
2026-02-27 12:28:46 -0500

    fix schema url

--- packages/sdk/codegen.clinical-api.ts
+++ packages/sdk/codegen.clinical-api.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://clinical-api.tau.health:8080/graphql',
+  schema: 'http://clinical-api.boson.health/graphql',
   // Only include documents that use clinical-api
   documents: ['./src/graphql/clinical-api/*.ts'],
   ignoreNoDocuments: true, // for better experience with the watcher
