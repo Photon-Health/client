4b4a45c3e00682b8354d1cbd188ce7cb483d2bd9
Morrison <nj.mrrsn@gmail.com>
2026-03-03 11:01:29 -0500

    Use a timeout to kickoff the welcome toast so the local storage flag isn't wiped immediately

--- apps/app/src/hooks/useWelcomeToast.tsx
+++ apps/app/src/hooks/useWelcomeToast.tsx
@@ -8,31 +8,36 @@ export function useWelcomeToast() {
   const toast = useToast();
 
   useEffect(() => {
-    console.log('Checked selfSignupInitialLogin', localStorage.getItem('selfSignupInitialLogin'));
-    const isInitialLogin = localStorage.getItem('selfSignupInitialLogin') === 'true';
+    const welcomeUser = () => {
+      console.log('Checked selfSignupInitialLogin', localStorage.getItem('selfSignupInitialLogin'));
+      const isInitialLogin = localStorage.getItem('selfSignupInitialLogin') === 'true';
 
-    if (user && isInitialLogin) {
-      console.log('Got user and its the first login!');
-      toast({
-        position: 'top',
-        duration: 5_000,
-        containerStyle: {
-          // for full width on mobile
-          width: { base: '95%', md: 'auto' },
-          maxWidth: { base: '95%', md: '24rem' }
-        },
-        render: ({ onClose }) => (
-          <StyledToast
-            onClose={onClose}
-            type="success"
-            title={`Welcome ${user.given_name}!`}
-            description="Add a patient to get started"
-          />
-        )
-      });
+      if (user && isInitialLogin) {
+        console.log('Got user and its the first login!');
+        toast({
+          position: 'top',
+          duration: 5_000,
+          containerStyle: {
+            // for full width on mobile
+            width: { base: '95%', md: 'auto' },
+            maxWidth: { base: '95%', md: '24rem' }
+          },
+          render: ({ onClose }) => (
+            <StyledToast
+              onClose={onClose}
+              type="success"
+              title={`Welcome ${user.given_name}!`}
+              description="Add a patient to get started"
+            />
+          )
+        });
+      }
 
       console.log('Clearing the local storage value!');
       localStorage.removeItem('selfSignupInitialLogin');
-    }
+    };
+
+    const welcomeTimeout = setTimeout(welcomeUser, 3_000);
+    return () => clearTimeout(welcomeTimeout);
   }, [toast, user]);
 }
