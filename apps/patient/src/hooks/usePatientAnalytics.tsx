dc3b68d9fb420d0dc93cca54b16e7d205c5296ba
zac-photon <zac@photon.health>
2026-03-25 17:30:14 -0400

    JSDoc(tor)

--- apps/patient/src/hooks/usePatientAnalytics.tsx
+++ apps/patient/src/hooks/usePatientAnalytics.tsx
@@ -12,6 +12,16 @@ interface PatientContextData {
 
 const PatientAnalyticsContext = createContext<PatientAnalytics | null>(null);
 
+/**
+ * This is a Patient-App-Wide Provider of a generic "PatientAnalytics" object,
+ * We can use the provider as a dependency injector of the different PatientAnalytics instances
+ * (Either NoOp or Rudder/MixPanel) into all the places we actually want to track data.
+ *
+ * The main thrust of putting this together was to encapsulate when and how we track our patient data.
+ * The simplest example being, if we're showing a demo we shouldn't be tracking anything.
+ * In the future this is an easy place to configure, change, or experiment with how and when we track
+ * patient behavior.
+ */
 export const PatientAnalyticsProvider = ({ children }: PatientAnalyticsProviderProps) => {
   const [searchParams] = useSearchParams();
 
