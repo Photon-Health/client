f81748ac64c0e26bc30df6f04b0b4466a04eb473
Midori Yang <midori@photon.health>
2026-03-26 15:05:51 -0400

    run playwright against deployed app

--- .github/scripts/add-e2e-test-checks.js
+++ .github/scripts/add-e2e-test-checks.js
@@ -2,7 +2,7 @@ module.exports = async ({ github, context }) => {
   const result = await github.rest.checks.listForRef({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    ref: `${process.env.SHA}`
+    ref: process.env.BRANCH_NAME
   });
   const testChecks = (result.data.check_runs || []).filter((check) =>
     check.name.includes('e2e-tests')
