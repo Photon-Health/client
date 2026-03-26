f24fb8d8c2a08130549b83c581137f977ce621c6
Midori Yang <midori@photon.health>
2026-03-26 15:37:51 -0400

    test splitting based on app

--- .github/scripts/add-e2e-test-checks.js
+++ .github/scripts/add-e2e-test-checks.js
@@ -1,17 +1,26 @@
 module.exports = async ({ github, context }) => {
+  const jobName = 'e2e-tests';
+  const appJobName = `${jobName} (${process.env.APP_NAME}`;
+
   const result = await github.rest.checks.listForRef({
     owner: context.repo.owner,
     repo: context.repo.repo,
     ref: process.env.BRANCH_NAME
   });
-  const testChecks = (result.data.check_runs || []).filter((check) =>
-    check.name.includes('e2e-tests')
-  );
 
+  const testChecks = (result.data.check_runs || []).filter((check) => check.name.includes(jobName));
   if (!testChecks.length) {
-    console.log('No e2e test checks found, aborting');
-    return;
+    throw new Error('No e2e test checks found, aborting');
   }
 
   console.log(testChecks);
+
+  console.log(appJobName);
+
+  const appCheck = testChecks.find((check) => check.name.includes(appJobName));
+  if (!appCheck) {
+    throw new Error(`No e2e test check for ${process.env.APP_NAME} found, aborting`);
+  }
+
+  console.log(appCheck);
 };
