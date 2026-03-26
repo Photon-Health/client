c05cec2e9aded0a7ae218f72e17fd9f49782dcbb
Midori Yang <midori@photon.health>
2026-03-26 14:06:05 -0400

    test checks API in e2e gha

---
+++ .github/scripts/add-e2e-test-checks.js
@@ -0,0 +1,17 @@
+module.exports = async ({ github, context }) => {
+  const result = await github.rest.checks.listForRef({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    ref: `${process.env.SHA}`
+  });
+  const testChecks = (result.data.check_runs || []).filter((check) =>
+    check.name.includes('e2e-tests')
+  );
+
+  if (!testChecks.length) {
+    console.log('No e2e test checks found, aborting');
+    return;
+  }
+
+  console.log(testChecks);
+};
