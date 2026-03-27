b2c89c43e9ea82cadc840278de1dba19b4269ea9
Midori Yang <midori@photon.health>
2026-03-27 11:23:37 -0400

    create check-in progress, try linking to workflow

--- .github/scripts/add-e2e-test-checks.js
+++ .github/scripts/add-e2e-test-checks.js
@@ -1,15 +1,36 @@
-module.exports = async ({ github, context }) => {
+const createCheck = async ({ github, context }) => {
   const payload = {
     owner: context.repo.owner,
     repo: context.repo.repo,
     name: `e2e-tests (${process.env.APP_NAME})`,
     head_sha: process.env.SHA,
-    status: 'completed',
-    conclusion: process.env.CONCLUSION,
-    details_url: process.env.DETAILS_URL
+    status: 'in_progress',
+    details_url: process.env.DETAILS_URL,
+    output: {
+      title: `E2E tests (${process.env.APP_NAME})`,
+      summary: `Workflow run can be found here: ${process.env.DETAILS_URL}`
+    }
   };
 
-  console.log('New check payload', JSON.stringify(payload, null, 2));
+  console.log('Payload', JSON.stringify(payload, null, 2));
 
-  await github.rest.checks.create(payload);
+  const result = await github.rest.checks.create(payload);
+  const checkId = result.data.id;
+  return checkId;
 };
+
+const updateCheck = async ({ github, context }) => {
+  const payload = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    check_run_id: process.env.CHECK_RUN_ID,
+    status: 'completed',
+    conclusion: process.env.CONCLUSION
+  };
+
+  console.log('Payload', JSON.stringify(payload, null, 2));
+
+  await github.rest.checks.update(payload);
+};
+
+module.exports = { createCheck, updateCheck };
