bb554eeedb73930b008485ce9eee7cc66f0841a7
Midori Yang <midori@photon.health>
2026-04-01 14:34:51 -0400

    test generating JWT

---
+++ tools/codebuild_workflow_dispatch.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+client_id=$1
+app_id=$2
+installation_id=$3
+private_key=$4
+
+# Copied from https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-json-web-token-jwt-for-a-github-app#example-using-bash-to-generate-a-jwt
+generate_jwt() {
+    local client_id=$1
+    local private_key=$2
+
+    b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
+
+    local now
+    now=$(date +%s)
+    local iat=$((now - 60)) # Issues 60 seconds in the past
+    local exp=$((now + 600)) # Expires 10 minutes in the future
+
+    local header_json='{
+        "typ":"JWT",
+        "alg":"RS256"
+    }'
+    # Header encode
+    local header
+    header=$( echo -n "${header_json}" | b64enc )
+
+    local payload_json="{
+        \"iat\":${iat},
+        \"exp\":${exp},
+        \"iss\":\"${client_id}\"
+    }"
+    # Payload encode
+    local payload
+    payload=$( echo -n "${payload_json}" | b64enc )
+
+    # Signature
+    local header_payload="${header}"."${payload}"
+    local tmp_key=$(mktemp)
+    printf '%b' "${private_key}" > "${tmp_key}"
+    local signature
+    signature=$(
+        echo -n "${header_payload}" | openssl dgst -sha256 -sign "${tmp_key}" | b64enc
+    )
+    rm -f "${tmp_key}"
+
+    # Create JWT
+    printf '%s' "${header_payload}"."${signature}"
+}
+
+JWT=$(generate_jwt "${client_id}" "${private_key}")
+printf '%s\n' "JWT: $JWT"
