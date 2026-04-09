73af3917266ffc305ae92792ffb275bd7386d3db
Scott De Silva <scott.desilva@photon.health>
2026-04-09 12:31:23 -0400

    masking provider info in session replays

--- apps/app/src/views/routes/Settings/components/organization/Organization.tsx
+++ apps/app/src/views/routes/Settings/components/organization/Organization.tsx
@@ -136,7 +136,9 @@ export const Organization = () => {
       ].map(({ title, value }) => ({
         title,
         value: value ? (
-          <Text fontSize="sm">{value}</Text>
+          <Text fontSize="sm" data-dd-privacy="mask" className="mp-mask">
+            {value}
+          </Text>
         ) : (
           <Text fontSize="sm" color="gray.400" as="i">
             Not available
