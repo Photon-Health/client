21d1a5af6fba51d729b6eb5fde397615eb0e3c64
Morrison <nj.mrrsn@gmail.com>
2026-04-17 12:55:56 -0400

    Remove feature flag for `change_pharmacy_reasons`. Add it as default behavior now

--- apps/patient/src/configs/featureFlags.ts
+++ apps/patient/src/configs/featureFlags.ts
@@ -1,5 +1,4 @@
 export type FlagValues = {
-  change_pharmacy_reasons: boolean;
   remove_review_your_rx_page: {
     skipReviewPage: boolean;
     showRxSummaryOnPharmacyPage: boolean;
@@ -9,7 +8,6 @@ export type FlagValues = {
 export type FlagKeys = keyof FlagValues;
 
 export const FEATURE_FLAG_DEFAULTS: FlagValues = {
-  change_pharmacy_reasons: false,
   remove_review_your_rx_page: {
     skipReviewPage: false,
     showRxSummaryOnPharmacyPage: false
