b302aa6270bcf80f47787412f089813ace8ab470
zac-photon <zac@photon.health>
2026-04-24 16:12:54 -0400

    fixer

--- apps/app/e2e/sso_login.spec.ts
+++ apps/app/e2e/sso_login.spec.ts
@@ -66,7 +66,7 @@ function expectSingleLogoutRedirect(navigationUrls: string[], connection: string
 }
 
 async function expectLandingPageVisible(page: Page) {
-  await expect(page.getByRole('heading', { name: 'New patient' })).toBeVisible({
+  await expect(page.getByRole('paragraph', { name: 'New patient' })).toBeVisible({
     timeout: 60_000
   });
 }
