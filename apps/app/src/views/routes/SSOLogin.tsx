ae3f5c23767abb0afab73b83cb8a5ecfd29225a8
zac-photon <zac@photon.health>
2026-04-27 11:59:07 -0400

    Fix regex

--- apps/app/src/views/routes/SSOLogin.tsx
+++ apps/app/src/views/routes/SSOLogin.tsx
@@ -83,8 +83,7 @@ function isCurrentOriginAllowed(): boolean {
       currentOrigin === 'http://localhost:3000' ||
       // Test for generated builds from PRs in the form of
       // app-[something here].boson.health
-      /^https:\/\/app(-.*)\.boson\.health$/.test(currentOrigin) ||
-      currentOrigin === 'https://app.boson.health' ||
+      /^https:\/\/app(-.*)?\.boson\.health$/.test(currentOrigin) ||
       currentOrigin === 'https://app.neutron.health' ||
       currentOrigin === 'https://app.photon.health'
     );
