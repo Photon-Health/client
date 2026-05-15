e53647968da6cbccf5282d99c0bff9e4eafd0767
scottdesilva-photon <scott.desilva@photon.health>
2026-05-15 14:43:29 -0400

    only show "next steps" for AP (#2678)
    * next steps copy just for ap
    
    * display pharmacy logo in order details
    
    * update copy
    
    * only show next steps for order places status
    
    * rename variable for clarity
    
    * rename another var

--- apps/patient/src/views/Status.tsx
+++ apps/patient/src/views/Status.tsx
@@ -251,7 +251,7 @@ export const Status = () => {
   );
 
   const pharmacyEstimatedReadyAt = useMemo(() => getLatestReadyTime(fulfillments), [fulfillments]);
-  const orderState = useMemo(
+  const fulfillmentState = useMemo(
     () =>
       order.fulfillment?.type === 'MAIL_ORDER'
         ? (order.fulfillment.state as 'FILLING' | 'SHIPPED' | 'DELIVERED')
@@ -276,6 +276,7 @@ export const Status = () => {
         pharmacyId={displayPharmacy?.id}
         pharmacyLogo={displayPharmacy?.logo}
         prescriptions={prescriptions}
+        fulfillmentState={fulfillmentState}
       />
       <Helmet>
         <title>{t.track}</title>
@@ -287,7 +288,7 @@ export const Status = () => {
               <HolidayAlert>Holiday may affect pharmacy hours.</HolidayAlert>
               <InsuranceAlert exception={unresolvedExceptions[0]?.exceptionType} />
               <OrderStatusHeader
-                status={orderState}
+                status={fulfillmentState}
                 fulfillmentType={order.fulfillment?.type}
                 integrated={order.pharmacy?.integrated}
                 pharmacyEstimatedReadyAt={pharmacyEstimatedReadyAt}
