25b6c2cbabab930d7ef9214ce42b44e656eea1fa
Scott De Silva <scott.desilva@photon.health>
2026-06-15 12:50:57 -0400

    Revert "hide setting pharmacy behind feature flag (#2718)"
    This reverts commit 308bfac5403ef1dc2909d993046ea585db35ae84.

--- apps/patient/src/configs/featureFlags.ts
+++ apps/patient/src/configs/featureFlags.ts
@@ -2,9 +2,6 @@ export type FlagValues = {
   remove_ready_by_selection_page: {
     skipReadyBySelectionPage: boolean;
   };
-  skip_preferred_pharmacy: {
-    isActive: boolean;
-  };
 };
 
 export type FlagKeys = keyof FlagValues;
@@ -12,8 +9,5 @@ export type FlagKeys = keyof FlagValues;
 export const FEATURE_FLAG_DEFAULTS: FlagValues = {
   remove_ready_by_selection_page: {
     skipReadyBySelectionPage: false
-  },
-  skip_preferred_pharmacy: {
-    isActive: false
   }
 };
